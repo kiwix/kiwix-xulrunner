2a21b5ccffe6a45b483ba16f2daf40623735d115
kelson42 <kelson42@users.sourceforge.net>
2007-09-15 17:12:43 +0000

    + usage()

--- indexer/kiwix/src/main.c
+++ indexer/kiwix/src/main.c
@@ -4,13 +4,19 @@
 #include "wordindex.h"
 #include "htmlbuffer.h"
 
+void usage() {
+  printf("Usage: ./kiwixindexer <path>\n");
+}
+
 int main(int argc, char **argv) {
 
-  static gchar defaultroot[] = "/home/fcoulon/devel/kiwix-devel/html";
-  char *root;
+  if (argc <= 1 ) {
+    usage();
+    exit(0);
+  }
 
-  if ( argc > 1 ) root = argv[1];
-  else root = defaultroot;
+  char *root = argv[1];
+
   printf( "Root = %s\n", root );
   wikiFniBuild( root );
   wikiBuildWordArticleMap( root );
