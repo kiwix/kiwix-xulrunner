e161c014a9d6577642f02712e8ed7d264ecddf78
reg_ <reg_@users.sourceforge.net>
2007-12-11 09:46:35 +0000

    added crawlers for some sub-projects in es

---
+++ indexer/moulin/crawler/es/crawler_source-category.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/ruby
+
+STARTTIME = Time.now
+
+# Global defines
+LANG    = "es"
+RTL     = false
+PROJECT = "source-category"
+MASTER	= "source"
+NAMESPAC= {:id => 14, :name => :NSCategory}
+UNIQID  = Time.now.to_i
+UNIQFD  = "/home/reg/var/#{LANG}/#{PROJECT}_#{UNIQID}"
+MDWKFD  = "/var/www/reg.kiwix.org/wiki"
+DBICON  = "DBI:Mysql:reg_#{LANG}_source"
+DBIUSER = "reg"
+DBIPASS = "reg"
+BLOCK_SIZE	= 10485760 # 10MB
+
+require 'es_common.rb'
+
+require '../crawler.rb'
+
+hello()
+
+puts "Duration: #{(Time.now - STARTTIME) /60} mn."
---
+++ indexer/moulin/crawler/es/crawler_source.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/ruby
+
+STARTTIME = Time.now
+
+# Global defines
+LANG    = "es"
+RTL     = false
+PROJECT = "source"
+MASTER = "source"
+NAMESPAC= {:id => 0, :name => nil}
+UNIQID  = Time.now.to_i
+UNIQFD  = "/home/reg/var/#{LANG}/#{PROJECT}_#{UNIQID}"
+MDWKFD  = "/var/www/reg.kiwix.org/wiki"
+DBICON  = "DBI:Mysql:reg_#{LANG}_#{PROJECT}"
+DBIUSER = "reg"
+DBIPASS = "reg"
+BLOCK_SIZE	= 10485760 # 10MB
+
+require './es_common.rb'
+
+require '../crawler.rb'
+
+hello()
+
+puts "Duration: #{(Time.now - STARTTIME) /60} mn."
