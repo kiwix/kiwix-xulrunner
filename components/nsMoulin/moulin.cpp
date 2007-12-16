664736fb026ad155b3f745214a98335aca1a5368
reg_ <reg_@users.sourceforge.net>
2007-12-16 14:53:44 +0000

    gecko 1.9 ready nsMoulin + docroot creator script

--- components/nsMoulin/moulin.cpp
+++ components/nsMoulin/moulin.cpp
@@ -18,12 +18,25 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
 
-#include "nsIGenericFactory.h"
+/*
+
+*/
 #include "bzlib.h"
 #include "zlib.h"
 #include "nsIMoulin.h"
 
-#include "plstr.h"
+#include "nsIGenericFactory.h"
+#include "nsStringAPI.h"
+//#include "nsString.h"
+#include "nspr.h"
+#include "xpcom-config.h"
+#include "nsMemory.h"
+#include "nsXPCOM.h"
+#include <stdio.h>
+
+
+/*
+
 #include "stdio.h"
 #include "nsCOMPtr.h"
 #include "nsMemory.h"
@@ -35,82 +48,44 @@
 #include "nsIProperties.h"
 #include "nsDirectoryServiceDefs.h"
 #include "nsEmbedString.h"
+*/
 
 // 759132FB-D0C5-42B7-B9AD-A7194A2494D3
 #define NS_MOULIN_CID \
 { 0x759132fb, 0xd0c5, 0x42B7, { 0xb9, 0xad, 0xa7, 0x19, 0x4a, 0x24, 0x94, 0xd3 } }
 #define NS_MOULIN_CONTRACTID "@kunnafoni.org/cpp_nsMoulin;1"
-class nsMoulinImpl : public nsIMoulin
+class nsMoulin : public nsIMoulin
 {
 public:
-    nsMoulinImpl( );
-    virtual ~nsMoulinImpl( );
-    NS_DECL_ISUPPORTS
-    NS_DECL_NSIMOULIN
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSIMOULIN
+
+  nsMoulin();
+
+private:
+  ~nsMoulin();
+
+protected:
+  /* additional members */
 };
 
-nsMoulinImpl::nsMoulinImpl( )
+
+NS_IMPL_ISUPPORTS1(nsMoulin, nsIMoulin)
+
+nsMoulin::nsMoulin()
 {
-    NS_INIT_ISUPPORTS();
-}
-nsMoulinImpl::~nsMoulinImpl( )
-{
-
-}
-
-NS_IMPL_ISUPPORTS1_CI(nsMoulinImpl, nsIMoulin);
-
-NS_IMETHODIMP
-nsMoulinImpl::RetrieveGzipContent(const char *archivefile, PRUint32 startoffset, PRInt32 length, nsACString & _retval)
-{
-	long     so = startoffset; //startoffset;
-	long     len = length; //length;
-	int     rem = 0;
-	int     i = 0;
-
-	int nl = so / 131072;
-	rem = so % 131072;
-
-	gzFile infile = gzopen(archivefile, "rb");
-
-	char buffer[131072];
-	char *buffer2;
-	if (len == -1) {
-		buffer2 = new char[131072];
-	 } else {
-		buffer2 = new char[len];
-	}
-	int num_read = 0;
-
-	while ( i <= nl + 1) {
-		if (i < nl) {
-			num_read = gzread(infile, buffer, sizeof(buffer));
-		}
-		if (i == nl + 1) {
-			num_read = gzread(infile, buffer, rem);
-		}
-		i++;
-	}
-	
-	if (len == -1) { // last article in ark
-		while (num_read != 0) {
-			num_read = gzread(infile, buffer2, 131072);
-			_retval.Append(buffer2, num_read);
-		}
-	} else {
-		num_read = gzread(infile, buffer2, len);
-		_retval.Append(buffer2, num_read);
-	}
-	
-	gzclose(infile);
-    
-	return NS_OK;
+  /* member initializers and constructor code */
 }
 
-NS_IMETHODIMP
-
-nsMoulinImpl::RetrieveBzip2Content(const char *archivefile, PRUint32 startoffset, PRInt32 length, nsACString & _retval)
+nsMoulin::~nsMoulin()
 {
+  /* destructor code */
+}
+
+/* AUTF8String retrieveBzip2Content (in string archivefile, in PRUint32 startoffset, in PRInt32 length); */
+NS_IMETHODIMP nsMoulin::RetrieveBzip2Content(const char *archivefile, PRUint32 startoffset, PRInt32 length, nsACString & _retval)
+{
+
 	FILE*   f;
 	BZFILE* b;
 	long     nBuf;
@@ -176,36 +151,74 @@ nsMoulinImpl::RetrieveBzip2Content(const char *archivefile, PRUint32 startoffset
 	return NS_OK;
 }
 
-NS_GENERIC_FACTORY_CONSTRUCTOR(nsMoulinImpl)
+/* AUTF8String retrieveGzipContent (in string archivefile, in PRUint32 startoffset, in PRInt32 length); */
+NS_IMETHODIMP nsMoulin::RetrieveGzipContent(const char *archivefile, PRUint32 startoffset, PRInt32 length, nsACString & _retval)
+{
+	long     so = startoffset; //startoffset;
+	long     len = length; //length;
+	int     rem = 0;
+	int     i = 0;
+
+	int nl = so / 131072;
+	rem = so % 131072;
+
+	gzFile infile = gzopen(archivefile, "rb");
+
+	char buffer[131072];
+	char *buffer2;
+	if (len == -1) {
+		buffer2 = new char[131072];
+	 } else {
+		buffer2 = new char[len];
+	}
+	int num_read = 0;
+
+	while ( i <= nl + 1) {
+		if (i < nl) {
+			num_read = gzread(infile, buffer, sizeof(buffer));
+		}
+		if (i == nl + 1) {
+			num_read = gzread(infile, buffer, rem);
+		}
+		i++;
+	}
+	
+	if (len == -1) { // last article in ark
+		while (num_read != 0) {
+			num_read = gzread(infile, buffer2, 131072);
+			_retval.Append(buffer2, num_read);
+		}
+	} else {
+		num_read = gzread(infile, buffer2, len);
+		_retval.Append(buffer2, num_read);
+	}
+	
+	gzclose(infile);
+    
+	return NS_OK;
+}
+
+
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsMoulin)
+
 static NS_METHOD nsMoulinRegistrationProc(nsIComponentManager *aCompMgr,
                                           nsIFile *aPath,
                                           const char *registryLocation,
                                           const char *componentType,
                                           const nsModuleComponentInfo *info)
-{
+ {
+    PR_fprintf(PR_STDOUT, "moulin component loaded\n");
     return NS_OK;
-}
-static NS_METHOD nsMoulinUnregistrationProc(nsIComponentManager *aCompMgr,
-                                            nsIFile *aPath,
-                                            const char *registryLocation,
-                                            const nsModuleComponentInfo *info)
-{
-    return NS_OK;
-}
-// For each class that wishes to support nsIClassInfo, add a line like this
-NS_DECL_CLASSINFO(nsMoulinImpl)
-static nsModuleComponentInfo components[ ] =
-{
-  { "moulin Component",    // a message to display when component is loaded
-    NS_MOULIN_CID,           // our UUID 
-    NS_MOULIN_CONTRACTID,    // our human readable PROGID or CLSID
-    nsMoulinImplConstructor,
-    nsMoulinRegistrationProc      /* NULL if you dont need one */,
-    nsMoulinUnregistrationProc    /* NULL if you dont need one */,
-    NULL /* no factory destructor */,
-    NS_CI_INTERFACE_GETTER_NAME(nsMoulinImpl),
-    NULL /* no language helper */,
-    &NS_CLASSINFO_NAME(nsMoulinImpl)
-  }
-};
+ }
+
+static nsModuleComponentInfo components[] =
+ {
+   {
+     "moulin Component", NS_MOULIN_CID, NS_MOULIN_CONTRACTID,
+     nsMoulinConstructor,
+     nsMoulinRegistrationProc
+   }
+ };
+
 NS_IMPL_NSGETMODULE(nsMoulinModule, components)
+
---
+++ indexer/moulin/crawler/create_docroot.rb
@@ -0,0 +1,80 @@
+#!/usr/bin/ruby
+
+usage	=<<END
+
+Usage:	./launcher.rb <lang>
+Ex:		./launcher.rb es
+END
+
+config	=<<END
+<?php
+
+$wgSitename         = "[[[TITLE]]]";
+$wgLanguageCode		= "[[[LANG]]]";
+
+$wgScriptPath       = "/wiki";
+if ($_ENV['CRAWLER'])
+    $wgArticlePath      = "moulin://[[[MASTER]]]/".$wgLanguageCode."/$1";
+else
+    $wgArticlePath      = "/".$wgLanguageCode."/[[[MASTER]]]/index.php/$1";
+
+$wgDBname           = "reg_".$wgLanguageCode."_[[[MASTER]]]";
+$wgDBuser           = "reg";
+$wgDBpassword       = "reg";
+
+$wgUploadDirectory	= "./images/".$wgLanguageCode;
+$wgUploadPath		= "/wiki/images/".$wgLanguageCode;
+$wgMathPath         = $wgUploadPath;
+$wgMathDirectory    = $wgUploadDirectory;
+$wgTmpDirectory     = $wgUploadDirectory;
+
+?>
+END
+
+index	=<<END
+<?php
+
+    $moulinLANG = "[[[LANG]]]/[[[PROJECT]]]";
+
+    chdir("../../wiki");
+    include("index.php");
+
+?>
+END
+
+raise usage if ARGV[0].nil?
+
+LANG	= ARGV[0]
+DOCROOT	= "/var/www/reg.kiwix.org"
+
+Dir.chdir(LANG)
+Dir.mkdir("#{DOCROOT}/#{LANG}") if not File.exists?("#{DOCROOT}/#{LANG}")
+#Dir.mkdir("../logs/#{LANG}") if not File.exists?("../logs/#{LANG}")
+
+projects = {}
+File.readlines("catalog").each do |l|
+	p = l.chomp.split(":")
+	next if p[0] == 'project_codes'
+	projects[p[0]] = p[1]
+end
+
+projects.each do |project, title|
+	tconfig = config
+	tindex	= index
+	master = project.split("-")[0]
+	[
+		{:s => "PROJECT", :r => project},
+		{:s => "MASTER", :r => master},
+		{:s => "LANG", :r => LANG},
+		{:s => "TITLE", :r => projects[master]}
+	].each do |p|
+		tconfig.gsub!("[[[#{p[:s]}]]]", p[:r])
+		tindex.gsub!("[[[#{p[:s]}]]]", p[:r])
+	end
+
+	Dir.mkdir("#{DOCROOT}/#{LANG}/#{project}") if not File.exists?("#{DOCROOT}/#{LANG}/#{project}")
+
+	File.open("#{DOCROOT}/#{LANG}/#{project}/config.php", "w") { |f| f.write tconfig }
+	File.open("#{DOCROOT}/#{LANG}/#{project}/index.php", "w") { |f| f.write tindex }
+
+end
