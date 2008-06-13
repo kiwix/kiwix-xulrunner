11c28418066047d16b24cdc7bc0ee59f54313c6f
yabause <yabause@users.sourceforge.net>
2008-06-13 09:59:42 +0000

    Adding Zeno component, based on Kiwix component, and Zeno protocol handler, based on Moulin protocol handler. I haven't commited the build/Makefile and components/Makefile as there's much chance Zeno component won't compile for now. (libzeno.a and libcxxtools.a are (for now) only found if installed in /usr/local/lib and zeno/*.h files must be in default include path)

---
+++ components/zenoProtocol/zenoProtocol.js
@@ -0,0 +1,216 @@
+/*  ZenoProtocol - Zeno protocol handler for Kiwix, offline reader of Wikipedia
+    based on moulin protocol handler
+    Copyright (C) 2008, LinterWeb (France), Guillaume Duhamel
+    Copyright (C) 2006-2007, Kunnafoni Foundation and contributors
+    
+    Contributor(s):
+        2004 Thomas Thurman <thomas@thurman.org.uk>
+        reg <reg@nurv.fr>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+
+const nsIProtocolHandler   = Components.interfaces.nsIProtocolHandler;
+const nsIURI               = Components.interfaces.nsIURI;
+const nsIStringInputStream = Components.interfaces.nsIStringInputStream;
+const nsIChannel           = Components.interfaces.nsIChannel;
+const nsIRequest           = Components.interfaces.nsIRequest;
+const nsISupports          = Components.interfaces.nsISupports;
+
+const ZENO_PROTOCOL_HANDLER_CID   = Components.ID("{a4d35b24-db6a-4522-929f-297723019229}");
+const ZENO_PROTOCOL_HANDLER_CTRID = "@mozilla.org/network/protocol;1?name=zeno";
+
+function ZenoChannel (uri)
+{
+  this.URI = uri;
+
+  this.originalURI = uri;
+  this._isPending = true; // why?
+  this.stringStream = Components.classes["@mozilla.org/io/string-input-stream;1"].createInstance(nsIStringInputStream);
+}
+
+ZenoChannel.prototype.QueryInterface =
+function jsdch_qi (iid)
+{
+  if (!iid.equals(nsIChannel) /* && !iid.equals(nsIRequest) (do we?) */ &&
+      !iid.equals(nsISupports)) {
+    throw Components.results.NS_ERROR_NO_INTERFACE;
+  }
+
+  return this;
+}
+
+/* nsIChannel */
+ZenoChannel.prototype.loadAttributes = null;
+ZenoChannel.prototype.contentType = "text/html";
+ZenoChannel.prototype.contentCharset = "UTF-8";
+ZenoChannel.prototype.contentLength = -1;
+ZenoChannel.prototype.owner = null;
+ZenoChannel.prototype.loadGroup = null;
+ZenoChannel.prototype.notificationCallbacks = null;
+ZenoChannel.prototype.securityInfo = null;
+
+ZenoChannel.prototype.open =
+function zenoch_open()
+{
+  // We don't have to provide this (should we, anyway?)
+  throw Components.results.NS_ERROR_NOT_IMPLEMENTED;
+}
+
+// The design of the following few functions is
+// based on that in Venkman.
+
+ZenoChannel.prototype.asyncOpen =
+function zenoch_aopen (streamListener, context, directURI)
+{
+	this.streamListener = streamListener;
+	this.context = context;
+	
+  	var zenocom = Components.classes["@linterweb.com/zeno"].getService();
+	zenocom = zenocom.QueryInterface(Components.interfaces.iZeno);
+	var contentType = new Object();
+	var data = zenocom.getArticle(this.URI, contentType);
+
+	this.contentType	= contentType.value;
+	this.charset	= "UTF-8";
+	this.contentLength	= data.length;
+
+	this.respond(data);
+}
+
+ZenoChannel.prototype.respond =
+function zenoch_respond (str)
+{
+    var storageStream = Components.classes["@mozilla.org/storagestream;1"].
+    		createInstance(Components.interfaces.nsIStorageStream);
+    var binaryStream = Components.classes["@mozilla.org/binaryoutputstream;1"].
+                createInstance(Components.interfaces.nsIBinaryOutputStream);
+    storageStream.init(512, 65536, null);
+    binaryStream.setOutputStream(storageStream);
+    binaryStream.writeBytes(str, this.contentLength);
+    this.stringStream = storageStream.newInputStream(0);
+
+    this.streamListener.onStartRequest (this, this.context);
+
+    this.streamListener.onDataAvailable (this, this.context, this.stringStream, 0, this.contentLength);
+    this.streamListener.onStopRequest (this, this.context, Components.results.NS_OK);
+
+    this._isPending = false;    
+}
+
+function ZenoProtocolHandler() {
+}
+
+
+ZenoProtocolHandler.prototype.scheme = "zeno";
+
+ZenoProtocolHandler.prototype.defaultPort = -1; 
+ZenoProtocolHandler.prototype.protocolFlags = nsIProtocolHandler.URI_NORELATIVE|nsIProtocolHandler.URI_NOAUTH;
+
+ZenoProtocolHandler.prototype.allowPort = function zeno_allowport (aPort, aScheme) { return false; }
+
+// Turns a string into an nsURI, possibly relative to
+// another nsURI.
+ZenoProtocolHandler.prototype.newURI =
+function zeno_newuri (spec, charset, baseURI)
+{
+	//L.info("asking newURI ("+spec.toString()+")");
+    var uri = Components.classes["@mozilla.org/network/simple-uri;1"]
+    		 .createInstance (Components.interfaces.nsIURI);
+   	// handle anchor links
+    if (baseURI instanceof nsIURI && spec.indexOf ("#") == 0) {
+		var sh = baseURI.spec.indexOf ("#");
+		if (sh != -1) {
+			var nb = baseURI.spec.substr (0, sh);
+			uri.spec = nb+spec; 
+		} else
+		   	uri.spec = baseURI.spec+spec;
+	} else {
+            var prefix = spec.indexOf("zeno://");
+	    if (prefix == -1) {
+	        uri.spec = "zeno://" + spec;
+	    } else {
+	        uri.spec = spec;
+            }
+	} 
+    return uri;
+}
+
+// Turns an nsURI into an nsIChannel
+ZenoProtocolHandler.prototype.newChannel =
+function zeno_newchannel (uri)
+{
+    return new ZenoChannel (uri);
+}
+
+////////////////////////////////////////////////////////////////
+// Zeno protocol handler factory.
+var ZenoProtocolHandlerFactory = new Object();
+
+ZenoProtocolHandlerFactory.createInstance =
+function zeno_phf_create(outer, iid)
+{
+  if (outer != null) {
+    dump("Don't squish us!\n");
+    throw Components.results.NS_ERROR_NO_AGGREGATION;
+  }
+
+  if (!iid.equals(nsIProtocolHandler) && !iid.equals(nsISupports)) {
+    dump("Ugh, weird interface.\n");
+    throw Components.results.NS_ERROR_INVALID_ARG;
+  }
+  
+  return new ZenoProtocolHandler();
+}
+
+////////////////////////////////////////////////////////////////
+// The Module
+var Module = new Object();
+
+Module.registerSelf =
+function mod_regself(compMgr, fileSpec, location, type) {
+  reg = compMgr.QueryInterface(Components.interfaces.nsIComponentRegistrar);
+  reg.registerFactoryLocation(ZENO_PROTOCOL_HANDLER_CID,
+			      "zeno protocol handler",
+			      ZENO_PROTOCOL_HANDLER_CTRID,
+			      fileSpec,
+			      location,
+			      type);
+}
+
+Module.getClassObject =
+function mod_getclassobj(compMgr, cid, iid) {
+  if (cid.equals(ZENO_PROTOCOL_HANDLER_CID)) {
+    return ZenoProtocolHandlerFactory;
+  }
+
+  // okay, so something's weird. give up.
+  
+  if (!iid.equals(Components.interfaces.nsIFactory))
+    throw Components.results.NS_ERROR_NOT_IMPLEMENTED;
+  
+  throw Components.results.NS_ERROR_NO_INTERFACE;
+  
+}
+
+Module.canUnload =
+function mod_canunload(compMgr) {
+  return true;
+}
+
+////////////////////////////////////////////////////////////////
+// NSGetModule
+function NSGetModule(compMgr, fileSpec) {
+  return Module;
+}
---
+++ components/zenocomponent/zenocomponent.cpp
@@ -0,0 +1,216 @@
+/*  ZenoComponent - XP-COM component for Kiwix, offline reader of Wikipedia
+    Copyright (C) 2007-2008, LinterWeb (France), Fabien Coulon, Guillaume Duhamel
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+
+#include "xpcom-config.h"
+#include "nsIGenericFactory.h"
+#include "IZeno.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "nsXPCOM.h"
+#include "nsEmbedString.h"
+#include "nsIURI.h"
+
+#include <zeno/files.h>
+#include <zeno/article.h>
+  
+#define ZENO_CID \
+{ 0xb2438c49, 0x8840, 0x48fa, \
+{ 0xaa, 0x59, 0x00, 0x4a, 0x8f, 0xe6, 0x46, 0x5f}}
+
+class Zeno : public iZeno
+{
+public:
+  NS_DECL_ISUPPORTS
+  NS_DECL_IZENO
+
+  Zeno();
+
+private:
+  ~Zeno();
+
+protected:
+  /* additional members */
+  zeno::Files * zenofiles;
+};
+
+/* Implementation file */
+NS_IMPL_ISUPPORTS1(Zeno, iZeno)
+
+Zeno::Zeno()
+{
+  /* member initializers and constructor code */
+
+    zenofiles = new zeno::Files("."); // FIXME
+}
+
+Zeno::~Zeno()
+{
+  /* destructor code */
+
+    if (zenofiles) delete zenofiles;
+}
+
+/* ACString getArticle (in nsIURI url, out string contentType); */
+NS_IMETHODIMP Zeno::GetArticle(nsIURI *url, char **contentType, nsACString & _retval)
+{
+    nsEmbedCString host;
+    url->GetPath(host);
+    const char * tmp = host.get();		// tmp + i == //Wikipedia/-/Hauptseite
+    char ns;
+    char title[1024];
+
+    unsigned int i = 0;
+    unsigned int n = strlen(tmp);
+
+    // default content type
+    *contentType = (char *) NS_Alloc(10);
+    strcpy(*contentType, "text/html");
+
+    // default retval
+    _retval = "";
+
+    // to fetch an article, we need its namespace and title...
+    while((i < n) and (tmp[i] == '/')) i++;	// tmp + i == Wikipedia/-/Hauptseite
+    while((i < n) and (tmp[i] != '/')) i++;	// tmp + i == /-/Hauptseite
+
+    while((i < n) and (tmp[i] == '/')) i++;	// tmp + i == -/Hauptseite
+    if (i == n)
+    {
+	return NS_OK;
+    }
+    // namespace
+    ns = tmp[i];
+    i++;					// tmp + i == /Hauptseite
+    if (tmp[i] != '/')
+    {
+	return NS_OK;
+    }
+    i++;					// tmp + i == Hauptseite
+
+    // title, we need to replace + by space and decode the url
+    unsigned int j = 0;
+    while((i < n) and (tmp[i] != '/'))
+    {
+        char c = tmp[i];
+	int decode1;
+	int decode2;
+	switch(c)
+	{
+	    case '+':
+	        title[j] = ' ';
+	        break;
+	    case '%':
+	        // Ok... this is wrong, each %XX code should be handled
+		// independently. Now the examples .zeno files are iso-8859-1(5)
+		// encoded and needs a title encoded the same way :/
+	        sscanf(tmp + i + 1, "%2x", &decode1);
+	        sscanf(tmp + i + 4, "%2x", &decode2);
+		title[j] = ((decode1 & 0x3) << 6) | (decode2 & 0x3F);
+		i += 5;
+	        break;
+	    default:
+	        title[j] = c;
+	        break;
+	}
+	j++;
+	i++;
+    }
+    title[j] = 0;
+
+    printf("title: %s\n", title);
+    
+    // now that we have the namespace and the title, we're ready to fetch the article
+    zeno::Article article = zenofiles->getArticle(ns, zeno::QUnicodeString(title));
+
+    // if there's no data in article, return (btw, getDataLen gives compressed data size)
+    if (article.getDataLen() == 0) return NS_OK;
+
+    // Now that we have something to return, set the correct mime type
+    NS_Free(*contentType);
+    std::string mimeType = article.getMimeType();
+    const char * mimeTypeC = mimeType.c_str();
+    *contentType = (char *) NS_Alloc(strlen(mimeTypeC) + 1);
+    strcpy(*contentType, mimeTypeC);
+
+    if (mimeType == "text/html")
+    {
+        // We need an extra work for text/html as the examples .zeno files are
+	// iso-8859-1(5) encoded... this part should be removed, or at least
+	// made optional
+        std::string content = article.getData();
+        const char * iso_content = content.c_str();
+        char * utf8_content;
+
+        n = strlen(iso_content);
+        utf8_content = (char *) NS_Alloc(2 * n + 1);
+        j = 0;
+        for(i = 0;i < n;i++)
+        {
+            unsigned char c = iso_content[i];
+	    if (c > 127)
+	    {
+                switch(c)
+	        {
+	            default:
+		        utf8_content[j] = ((c & 0xC0) >> 6) | 0xC0;
+		        utf8_content[j + 1] = (c & 0x3F) | 0x80;
+	        }
+	        j += 2;
+	    }
+	    else
+	    {
+                utf8_content[j] = iso_content[i];
+                j++;
+	    }
+        }
+        utf8_content[j] = 0;
+
+	_retval = nsDependentCString(utf8_content, j + 1);
+
+        NS_Free(utf8_content);
+    }
+    else
+    {
+        std::string content = article.getData();
+	std::string::size_type n = content.size();
+
+        char * buffer = (char *) NS_Alloc(n + 1);
+        // not using strcpy as data can contains 0
+	for(i = 0;i < n;i++)
+	{
+	    buffer[i] = content[i];
+	}
+
+	_retval = nsDependentCString(buffer, i);
+    }
+
+    return NS_OK;
+}
+
+NS_GENERIC_FACTORY_CONSTRUCTOR(Zeno)
+
+static const nsModuleComponentInfo components[] =
+{
+   { "Zeno",
+     ZENO_CID,
+     "@linterweb.com/zeno",
+     ZenoConstructor
+   }
+};
+
+NS_IMPL_NSGETMODULE(nsZenoModule, components)
