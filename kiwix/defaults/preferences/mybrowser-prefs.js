c0a8f63165534c26cbbc4e70b8f7249109f2bba9
kelson42 <kelson42@users.sourceforge.net>
2009-01-25 10:20:45 +0000

    + new prefs

--- kiwix/defaults/preferences/mybrowser-prefs.js
+++ kiwix/defaults/preferences/mybrowser-prefs.js
@@ -1,4 +1,20 @@
 pref("toolkit.defaultChromeURI", "chrome://interfacewiki/content/interfacewiki.xul");
 pref("general.skins.selectedSkin", "classicBlue");
 pref("intl.charset.default", "UTF-8");
+
+pref("intl.locale.matchOS", true);
 pref("network.protocol-handler.warn-external.http", false);
+pref("corpus.url", "http://download.kiwix.org/kiwix/");
+pref("corpus.destination", "");
+
+pref("app.update.enabled", true);
+pref("app.update.auto", true);
+pref("app.update.mode", 3);
+pref("app.update.silent", false);
+pref("app.update.url", "http://download.kiwix.org/update/%PRODUCT%/%VERSION%/%BUILD_ID%/%BUILD_TARGET%/update.xml");
+pref("app.update.interval", 86400);
+pref("app.update.nagTimer.download", 86400);
+pref("app.update.nagTimer.restart", 1800);
+pref("app.update.timer", 600000);
+pref("app.update.showInstalledUI", true);
+pref("app.update.incompatible.mode", 0);
