82ecf90874b353d861d42ffd47700961a0eeb70c
kelson42 <kelson42@users.sourceforge.net>
2009-02-12 20:26:50 +0000

    fully rewrited protocol handler for binary contents

--- components/zenoProtocol/zenoProtocol.js
+++ components/zenoProtocol/zenoProtocol.js
@@ -1,139 +1,29 @@
-/*  ZenoProtocol - Zeno protocol handler for Kiwix, offline reader of Wikipedia
-    based on moulin protocol handler
-    Copyright (C) 2008, LinterWeb (France), Guillaume Duhamel
-    Copyright (C) 2006-2007, Kunnafoni Foundation and contributors
-    
-    Contributor(s):
-        2004 Thomas Thurman <thomas@thurman.org.uk>
-        reg <reg@nurv.fr>
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+const Cr = Components.results;
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+var zenoFile = '/var/www/dumps/kiwix_0.5.zeno';
+var zenoAccessor = null;
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+/*** ZenoprotocolHandler ***/
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+function ZenoprotocolHandler() {}
 
-const nsIProtocolHandler   = Components.interfaces.nsIProtocolHandler;
-const nsIURI               = Components.interfaces.nsIURI;
-const nsIStringInputStream = Components.interfaces.nsIStringInputStream;
-const nsIChannel           = Components.interfaces.nsIChannel;
-const nsIRequest           = Components.interfaces.nsIRequest;
-const nsISupports          = Components.interfaces.nsISupports;
+ZenoprotocolHandler.prototype = {
+    defaultPort: -1,
+    protocolFlags: Ci.nsIProtocolHandler.URI_NORELATIVE,
+    allowPort: function(port, scheme) { return false; },
+    newURI: function(spec, charset, baseURI) {
+	//        var uri = Cc["@mozilla.org/network/simple-uri;1"].createInstance(Ci.nsIURI);
+	//        uri.spec = spec;
+	//        return uri;
 
-const ZENO_PROTOCOL_HANDLER_CID   = Components.ID("{a4d35b24-db6a-4522-929f-297723019229}");
-const ZENO_PROTOCOL_HANDLER_CTRID = "@mozilla.org/network/protocol;1?name=zeno";
-
-function ZenoChannel (uri)
-{
-  this.URI = uri;
-
-  this.originalURI = uri;
-  this._isPending = true; // why?
-  this.stringStream = Components.classes["@mozilla.org/io/string-input-stream;1"].createInstance(nsIStringInputStream);
-}
-
-ZenoChannel.prototype.QueryInterface =
-function jsdch_qi (iid)
-{
-  if (!iid.equals(nsIChannel) /* && !iid.equals(nsIRequest) (do we?) */ &&
-      !iid.equals(nsISupports)) {
-    throw Components.results.NS_ERROR_NO_INTERFACE;
-  }
-
-  return this;
-}
-
-/* nsIChannel */
-ZenoChannel.prototype.loadAttributes = null;
-ZenoChannel.prototype.contentType = "text/html";
-ZenoChannel.prototype.contentCharset = "UTF-8";
-ZenoChannel.prototype.contentLength = -1;
-ZenoChannel.prototype.owner = null;
-ZenoChannel.prototype.loadGroup = null;
-ZenoChannel.prototype.notificationCallbacks = null;
-ZenoChannel.prototype.securityInfo = null;
-
-ZenoChannel.prototype.open =
-function zenoch_open()
-{
-  // We don't have to provide this (should we, anyway?)
-  throw Components.results.NS_ERROR_NOT_IMPLEMENTED;
-}
-
-// The design of the following few functions is
-// based on that in Venkman.
-
-ZenoChannel.prototype.asyncOpen =
-function zenoch_aopen (streamListener, context, directURI)
-{
-	this.streamListener = streamListener;
-	this.context = context;
-	
-  	var zenocom = Components.classes["@linterweb.com/zeno"].getService();
-	zenocom = zenocom.QueryInterface(Components.interfaces.iZeno);
-	var contentType = new Object();
-	var data = zenocom.getArticle(this.URI, contentType);
-
-	this.contentType	= contentType.value;
-	this.charset	= "UTF-8";
-	this.contentLength	= data.length;
-
-	if (data.length == 0) {
-		throw 0x80520012;
-	} else {
-		this.respond(data);
-	}
-}
-
-ZenoChannel.prototype.respond =
-function zenoch_respond (str)
-{
-    var storageStream = Components.classes["@mozilla.org/storagestream;1"].
-    		createInstance(Components.interfaces.nsIStorageStream);
-    var binaryStream = Components.classes["@mozilla.org/binaryoutputstream;1"].
-                createInstance(Components.interfaces.nsIBinaryOutputStream);
-    storageStream.init(512, this.contentLength, null);
-    binaryStream.setOutputStream(storageStream);
-    binaryStream.writeBytes(str, this.contentLength);
-    this.stringStream = storageStream.newInputStream(0);
-
-    this.streamListener.onStartRequest (this, this.context);
-    this.streamListener.onDataAvailable (this, this.context, this.stringStream, 0, this.contentLength);
-    this.streamListener.onStopRequest (this, this.context, Components.results.NS_OK);
-
-    this._isPending = false;    
-}
-
-function ZenoProtocolHandler() {
-}
-
-
-ZenoProtocolHandler.prototype.scheme = "zeno";
-
-ZenoProtocolHandler.prototype.defaultPort = -1; 
-ZenoProtocolHandler.prototype.protocolFlags = nsIProtocolHandler.URI_NORELATIVE|nsIProtocolHandler.URI_NOAUTH;
-
-ZenoProtocolHandler.prototype.allowPort = function zeno_allowport (aPort, aScheme) { return false; }
-
-// Turns a string into an nsURI, possibly relative to
-// another nsURI.
-ZenoProtocolHandler.prototype.newURI =
-function zeno_newuri (spec, charset, baseURI)
-{
 
 	//L.info("asking newURI ("+spec.toString()+")");
     var uri = Components.classes["@mozilla.org/network/simple-uri;1"]
     		 .createInstance (Components.interfaces.nsIURI);
    	// handle anchor links
-    if (baseURI instanceof nsIURI && spec.indexOf ("#") == 0) {
+    if (baseURI instanceof Components.interfaces.nsIURI && spec.indexOf ("#") == 0) {
 		var sh = baseURI.spec.indexOf ("#");
 		if (sh != -1) {
 			var nb = baseURI.spec.substr (0, sh);
@@ -148,74 +38,184 @@ function zeno_newuri (spec, charset, baseURI)
 	        uri.spec = spec;
             }
 	} 
+    
     uri.spec = uri.spec.replace(/\+/g, "%2B");
     return uri;
+
+    },
+    newChannel: function(URI) {
+        var channel = new PipeChannel(URI);
+        return channel.QueryInterface(Ci.nsIChannel);
+    },
+    QueryInterface: function(iid) {
+        if(!iid.equals(Ci.nsIProtocolHandler) && !iid.equals
+(Ci.nsISupports)) throw Cr.NS_ERROR_NO_INTERFACE;
+        return this;
+    }
+
 }
 
-// Turns an nsURI into an nsIChannel
-ZenoProtocolHandler.prototype.newChannel =
-function zeno_newchannel (uri)
-{
-    return new ZenoChannel (uri);
+/**********************************************************************/
+/*** PipeChannel ***/
+
+var PipeChannel = function(URI) {
+  this.pipe = Cc["@mozilla.org/pipe;1"].createInstance(Ci.nsIPipe);
+  this.pipe.init(true,true,0,0,null);
+  this.inputStreamChannel = Cc["@mozilla.org/network/input-stream-channel;1"].createInstance(Ci.nsIInputStreamChannel);
+  this.inputStreamChannel.setURI(URI);
+  this.inputStreamChannel.contentStream = this.pipe.inputStream;
+  this.request = this.inputStreamChannel.QueryInterface
+(Ci.nsIRequest);
+  this.channel = this.inputStreamChannel.QueryInterface
+(Ci.nsIChannel);
+
 }
 
-////////////////////////////////////////////////////////////////
-// Zeno protocol handler factory.
-var ZenoProtocolHandlerFactory = new Object();
+PipeChannel.prototype = {
+  QueryInterface: function(iid){
+    if (iid.equals(Ci.nsIChannel) || iid.equals(Ci.nsIRequest) ||
+iid.equals(Ci.nsISupports))
+      return this;
+    throw Cr.NS_NOINTERFACE;
+  },
 
-ZenoProtocolHandlerFactory.createInstance =
-function zeno_phf_create(outer, iid)
-{
-  if (outer != null) {
-    dump("Don't squish us!\n");
-    throw Components.results.NS_ERROR_NO_AGGREGATION;
+  get LOAD_NORMAL() {return this.request.LOAD_NORMAL},
+  get LOAD_BACKGROUND() {return this.request.LOAD_BACKGROUND},
+  get INHIBIT_CACHING() {return this.request.INHIBIT_CACHING},
+  get INHIBIT_PERSISTENT_CACHING() {return
+this.request.INHIBIT_PERSISTENT_CACHING},
+  get LOAD_BYPASS_CACHE() {return this.request.LOAD_BYPASS_CACHE},
+  get LOAD_FROM_CACHE() {return this.request.LOAD_FROM_CACHE},
+  get VALIDATE_ALWAYS() {return this.request.VALIDATE_ALWAYS},
+  get VALIDATE_NEVER() {return this.request.VALIDATE_NEVER},
+  get VALIDATE_ONCE_PER_SESSION() {return
+this.request.VALIDATE_ONCE_PER_SESSION},
+
+  get loadFlags() {return this.request.loadFlags},
+  set loadFlags(val) {this.request.loadFlags = val},
+  get loadGroup() {return this.request.loadGroup},
+  set loadGroup(val) {this.request.loadGroup = val},
+  get name() {return this.request.name},
+  get status() {return this.request.status},
+
+  cancel: function(status) {this.request.cancel(status);},
+  isPending: function() {return this.request.isPending();},
+  resume: function() {this.request.resume();},
+  suspend: function() {this.request.suspend();},
+
+  get LOAD_DOCUMENT_URI() {return this.channel.LOAD_DOCUMENT_URI},
+  get LOAD_RETARGETED_DOCUMENT_URI() {return
+this.channel.LOAD_RETARGETED_DOCUMENT_URI},
+  get LOAD_REPLACE() {return this.channel.LOAD_REPLACE},
+  get LOAD_INITIAL_DOCUMENT_URI() {return
+this.channel.LOAD_INITIAL_DOCUMENT_URI},
+  get LOAD_TARGETED() {return this.channel.LOAD_TARGETED},
+
+  get contentCharset() {return this.channel.contentCharset},
+  set contentCharset(val) {this.channel.contentCharset = val},
+  get contentLength() {return this.channel.contentLength},
+  set contentLength(val) {this.channel.contentLength = val},
+  get contentType() {return this.channel.contentType},
+  set contentType(val) {this.channel.contentType = val},
+  get notificationCallbacks() {return
+this.channel.notificationCallbacks},
+  set notificationCallbacks(val) {this.channel.notificationCallbacks =
+val},
+  get originalURI() {return this.channel.originalURI},
+  set originalURI(val) {this.channel.originalURI = val},
+  get owner() {return this.channel.owner},
+  set owner(val) {this.channel.owner = val},
+  get securityInfo() {return this.channel.securityInfo},
+  get URI() {return this.channel.URI},
+
+  asyncOpen: function(listener, context) {
+    this.channel.asyncOpen(listener, context);
+
+    try {
+        if(false/* some reason to abort */) {
+            this.request.cancel(Cr.NS_BINDING_FAILED);
+            Cc["@mozilla.org/embedcomp/prompt-service;1"]
+                .getService(Ci.nsIPromptService)
+                .alert(null, 'Error message.', 'Error message.');
+            return;
+        }
+	
+	/* load the zeno file if necessary */
+	if (zenoAccessor == null) {
+	    zenoAccessor = Components.classes["@kiwix.org/zenoAccessor"].getService();
+	    zenoAccessor = zenoAccessor.QueryInterface(Components.interfaces.IZenoAccessor);
+	    zenoAccessor.loadFile(zenoFile);
+	}
+
+	var contentType = new Object();
+	result = zenoAccessor.getContent(this.URI, contentType);
+
+        this.pipe.outputStream.write(result,result.length);
+        this.pipe.outputStream.close();
+    } catch(err) {
+      if (err.result != Cr.NS_BINDING_ABORTED) {
+        Components.utils.reportError(err);
+      }
+    }
+  },
+
+  open: function() {return this.channel.open();},
+
+  close: function() {
+    this.pipe.outputStream.close();
   }
 
-  if (!iid.equals(nsIProtocolHandler) && !iid.equals(nsISupports)) {
-    dump("Ugh, weird interface.\n");
-    throw Components.results.NS_ERROR_INVALID_ARG;
-  }
-  
-  return new ZenoProtocolHandler();
 }
 
-////////////////////////////////////////////////////////////////
-// The Module
-var Module = new Object();
+/**********************************************************************/
+/*** ZenoprotocolHandlerFactory ***/
 
-Module.registerSelf =
-function mod_regself(compMgr, fileSpec, location, type) {
-  reg = compMgr.QueryInterface(Components.interfaces.nsIComponentRegistrar);
-  reg.registerFactoryLocation(ZENO_PROTOCOL_HANDLER_CID,
-			      "zeno protocol handler",
-			      ZENO_PROTOCOL_HANDLER_CTRID,
-			      fileSpec,
-			      location,
-			      type);
-}
+var ZenoprotocolHandlerFactory = {
+  createInstance: function(outer, iid) {
+    if(outer != null) throw Cr.NS_ERROR_NO_AGGREGATION;
+    return (new ZenoprotocolHandler()).QueryInterface(iid);
+  },
 
-Module.getClassObject =
-function mod_getclassobj(compMgr, cid, iid) {
-  if (cid.equals(ZENO_PROTOCOL_HANDLER_CID)) {
-    return ZenoProtocolHandlerFactory;
+  QueryInterface: function(iid) {
+    if(!iid.equals(Ci.nsIFactory) && !iid.equals(Ci.nsISupports))
+throw Cr.NS_ERROR_NO_INTERFACE;
+    return this;
   }
 
-  // okay, so something's weird. give up.
-  
-  if (!iid.equals(Components.interfaces.nsIFactory))
-    throw Components.results.NS_ERROR_NOT_IMPLEMENTED;
-  
-  throw Components.results.NS_ERROR_NO_INTERFACE;
-  
 }
 
-Module.canUnload =
-function mod_canunload(compMgr) {
+/**********************************************************************/
+/*** ZenoprotocolModule ***/
+
+var ZenoprotocolModule = new Object();
+
+ZenoprotocolModule.registerSelf = function(compMgr, fileSpec, location, type) {
+  compMgr = compMgr.QueryInterface(Ci.nsIComponentRegistrar);
+  compMgr.registerFactoryLocation(Components.ID("{ee042780-dcf9-11dd-8733-0002a5d5c51b}"),
+                                  "Zeno protocol handler",
+                                  "@mozilla.org/network/protocol;1?name=zeno",
+                                  fileSpec, location, type);
+
+}
+
+ZenoprotocolModule.unregisterSelf = function(compMgr, location, loaderStr) {
+  compMgr = compMgr.QueryInterface(Ci.nsIComponentRegistrar);
+  compMgr.unregisterFactoryLocation(Components.ID("{ee042780-dcf9-11dd-8733-0002a5d5c51b}"), location);
+
+}
+
+ZenoprotocolModule.getClassObject = function(compMgr, cid, iid) {
+  if(!iid.equals(Ci.nsIFactory)) throw Cr.NS_ERROR_NOT_IMPLEMENTED;
+  if(cid.equals(Components.ID("{ee042780-dcf9-11dd-8733-0002a5d5c51b}"))) return ZenoprotocolHandlerFactory;
+  throw Cr.NS_ERROR_NO_INTERFACE;
+
+}
+
+ZenoprotocolModule.canUnload = function(compMgr) {
   return true;
+
 }
 
-////////////////////////////////////////////////////////////////
-// NSGetModule
 function NSGetModule(compMgr, fileSpec) {
-  return Module;
+  return ZenoprotocolModule;
 }
