74455c15690bda559e86308a003099ebe85ade0d
kelson42 <kelson42@users.sourceforge.net>
2009-02-14 18:01:22 +0000

    + first version allowing indexation

--- components/xapianAccessor/xapianAccessor.cpp
+++ components/xapianAccessor/xapianAccessor.cpp
@@ -3,6 +3,7 @@
 #include "IXapianAccessor.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "nsXPCOM.h"
 #include "nsEmbedString.h"
@@ -17,6 +18,11 @@
 #include <zeno/file.h>
 #include <zeno/article.h>
   
+#include <xapian.h>
+#include <xapian/myhtmlparse.h>
+
+using namespace std;
+
 class XapianAccessor : public IXapianAccessor {
 
 public:
@@ -27,6 +33,11 @@ public:
 
 private:
   ~XapianAccessor();
+
+  MyHtmlParser htmlParser;
+  Xapian::WritableDatabase database;
+  Xapian::Stem stemmer;
+  Xapian::TermGenerator indexer;
 };
 
 /* Implementation file */
@@ -39,8 +50,62 @@ XapianAccessor::XapianAccessor() {}
 XapianAccessor::~XapianAccessor() {
 }
 
+/* Open Xapian database */
+NS_IMETHODIMP XapianAccessor::OpenDatabase(const char* directory, PRBool *retVal) {
+  try {
+    this->database = Xapian::WritableDatabase(directory, Xapian::DB_CREATE_OR_OVERWRITE);
+  } catch(...) {
+  }
+
+  *retVal = PR_TRUE;
+}
+
+/* Open Xapian database */
+NS_IMETHODIMP XapianAccessor::CloseDatabase( PRBool *retVal) {
+  try {
+    this->database.flush();
+  } catch(...) {
+  }
+
+  *retVal = PR_TRUE;
+}
+
 /* Create xapian db from zeno file */
-NS_IMETHODIMP XapianAccessor::CreateDb(const char *zenoFilepath, const char *xapianDirectoryPath, nsACString &_retval) {
+NS_IMETHODIMP XapianAccessor::AddArticleToDatabase(const char *url, const char *content, PRBool *retVal) {
+  this->htmlParser.reset();
+
+  try {
+    this->htmlParser.parse_html(content, "UTF-8", true);
+  } catch(...) {
+  }
+
+  /*
+  std::cout << "Title:" << this->htmlParser.title << "\n";
+  std::cout << "Keywords:" << this->htmlParser.keywords << "\n";
+  std::cout << "Dump:" << this->htmlParser.dump << "\n";
+  std::cout << "Sample:" << this->htmlParser.sample << "\n";
+  */
+
+  // Put the data in the document
+  Xapian::Document document;
+  document.set_data(this->htmlParser.title);
+  
+  // Index the title
+  indexer.set_document(document);
+  if (!this->htmlParser.title.empty()) {
+    indexer.index_text(this->htmlParser.title, 2);
+    indexer.increase_termpos(100);
+  }
+  
+  // Index the content
+  if (!this->htmlParser.dump.empty()) {
+    indexer.index_text(this->htmlParser.dump);
+  }
+
+  // add to the database
+  this->database.add_document(document);
+
+  *retVal = PR_TRUE;
 }
 
 NS_GENERIC_FACTORY_CONSTRUCTOR(XapianAccessor)
