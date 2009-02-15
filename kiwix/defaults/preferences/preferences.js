0cd4eb7357cda720d004617985d8193447885722
kelson42 <kelson42@users.sourceforge.net>
2009-02-15 16:29:48 +0000

    +comments

--- kiwix/defaults/preferences/preferences.js
+++ kiwix/defaults/preferences/preferences.js
@@ -1,25 +1,14 @@
-pref("toolkit.defaultChromeURI", "chrome://interfacewiki/content/interfacewiki.xul");
-pref("general.skins.selectedSkin", "classicBlue");
+/* Start XUL page */
+pref("toolkit.defaultChromeURI", "chrome://main/content/main.xul");
+
+/* Default skin */
+pref("general.skins.selectedSkin", "default");
+
+/* Charset */
 pref("intl.charset.default", "UTF-8");
 
-pref("intl.locale.matchOS", true);
-pref("network.protocol-handler.warn-external.http", false);
-pref("corpus.url", "http://download.kiwix.org/kiwix/");
-pref("corpus.destination", "");
-
-pref("app.update.enabled", true);
-pref("app.update.auto", true);
-pref("app.update.mode", 3);
-pref("app.update.silent", false);
-pref("app.update.url", "http://download.kiwix.org/update/%PRODUCT%/%VERSION%/%BUILD_ID%/%BUILD_TARGET%/update.xml");
-pref("app.update.interval", 86400);
-pref("app.update.nagTimer.download", 86400);
-pref("app.update.nagTimer.restart", 1800);
-pref("app.update.timer", 600000);
-pref("app.update.showInstalledUI", true);
-pref("app.update.incompatible.mode", 0);
-
+/* Debug preferences */
 pref("javascript.options.showInConsole", true);
 pref("browser.dom.window.dump.enabled", true);
-//pref("javascript.options.strict", true);
+pref("javascript.options.strict", true);
 pref("extensions.logging.enabled", true);
