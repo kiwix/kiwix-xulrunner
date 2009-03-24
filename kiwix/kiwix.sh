ba846b084026b53730d7aee4d96111044a766e59
kelson42 <kelson42@users.sourceforge.net>
2009-03-24 19:10:33 +0000

    + remove few bugs

--- kiwix/kiwix.sh
+++ kiwix/kiwix.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 # Take a look to the current directory
-XULRUNNER=`find ./ -name xulrunner`
+XULRUNNER=`find ./ -executable -type f -name xulrunner`
 
 # If no xulrunner, get the path of the xulrunner install of the system
-if [ ! "$XULRUNNER" ]
+if [ !"$XULRUNNER" ]
 then
     XULRUNNER=`whereis xulrunner | cut -d" " -f2`
 fi
 
-# Ff no result print a message
+# If no result print a message
 if [ ! "$XULRUNNER" ] || [ ! -f "$XULRUNNER" ]
 then
     echo "'xulrunner' is not installed, you have to install it to use Kiwix."
