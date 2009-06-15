4bdcb90e915a880964fff3c57cb65f20e4c6aa56
kelson42 <kelson42@users.sourceforge.net>
2009-06-15 14:44:21 +0000

    stub

---
+++ build-kiwix-release.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+# Go the the /tmp directory
+cd /tmp
+
+# Download code
+svn co https://kiwix.svn.sourceforge.net/svnroot/kiwix/moulinkiwix moulinkiwix
+
+# Get and compile the dependences
+cd ./moulinkiwix/dependences
+make
+
+# Compile the components
+cd ../components
+cd zimAccessor ; ./autogen.sh ; ./configure ; make clean all
+cd ../xapianAccessor ; ./autogen.sh ; ./configure ; make clean all
+cd ../zimXapianIndexer ; ./autogen.sh ; ./configure ; make clean all
+
+# Copy the kiwix directory
+cd /tmp
+cp -r -L /tmp/moulinkiwix/kiwix .
+
+# Remove svn stuff
+for i in `find ./kiwix -name ".svn"` ; do rm -rf $i ; done
+
+# Download and copy xulrunner
+wget http://releases.mozilla.org/pub/mozilla.org/xulrunner/releases/1.9.0.11/runtimes/xulrunner-1.9.0.11.en-US.linux-i686.tar.bz2
+tar -xvjf xulrunner-1.9.0.11.en-US.linux-i686.tar.bz2
+mv ./xulrunner ./kiwix/
+
+# Download the firefox en copy the locales JAR
+rm firefox-3.5b99.tar.bz2
+wget -c http://releases.mozilla.org/pub/mozilla.org/firefox/releases/latest-3.5/linux-i686/it/firefox-3.5b99.tar.bz2
+rm -rf ./firefox
+tar -xvjf firefox-3.5b99.tar.bz2
+cp ./firefox/chrome/it.* ./kiwix/xulrunner/chrome/
+
+rm firefox-3.5b99.tar.bz2
+wget -c http://releases.mozilla.org/pub/mozilla.org/firefox/releases/latest-3.5/linux-i686/fr/firefox-3.5b99.tar.bz2
+rm -rf ./firefox
+tar -xvjf firefox-3.5b99.tar.bz2
+cp ./firefox/chrome/fr.* ./kiwix/xulrunner/chrome/
+
+rm firefox-3.5b99.tar.bz2
+wget -c http://releases.mozilla.org/pub/mozilla.org/firefox/releases/latest-3.5/linux-i686/es-ES/firefox-3.5b99.tar.bz2
+rm -rf ./firefox
+tar -xvjf firefox-3.5b99.tar.bz2
+cp ./firefox/chrome/es-ES.* ./kiwix/xulrunner/chrome/
+
+rm firefox-3.5b99.tar.bz2
+wget -c http://releases.mozilla.org/pub/mozilla.org/firefox/releases/latest-3.5/linux-i686/de/firefox-3.5b99.tar.bz2
+rm -rf ./firefox
+tar -xvjf firefox-3.5b99.tar.bz2
+cp ./firefox/chrome/de.* ./kiwix/xulrunner/chrome/
+
+# Tar & clean
+tar -cvjf kiwix.tar.bz2 ./kiwix
+rm -rf ./firefox*
+rm -rf ./xulrunner*
+rm -rf ./moulinkiwix*
+rm -rf ./kiwix/
