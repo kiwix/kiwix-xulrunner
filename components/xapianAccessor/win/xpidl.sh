add61d78685e809e7519719fc537df6367e1381a
kelson42 <kelson42@users.sourceforge.net>
2009-07-27 19:59:53 +0000

    improvemntso fhte xapianAccessor win build script

---
+++ components/xapianAccessor/win/xpidl.sh
@@ -0,0 +1,3 @@
+../../../dependences/win/xulrunner-sdk/bin/xpidl.exe -m header -I../../../dependences/win/xulrunner-sdk/idl ../IXapianAccessor.idl
+../../../dependences/win/xulrunner-sdk/bin/xpidl.exe -m typelib -I../../../dependences/win/xulrunner-sdk/idl ../IXapianAccessor.idl
+mv IXapianAccessor.* ../
