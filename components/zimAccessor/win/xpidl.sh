17e61bd6fef006fca26e98ea41f9213162db4c72
kelson42 <kelson42@users.sourceforge.net>
2009-08-01 16:36:34 +0000

    + imp. of windows  zimAccessor compil. scripts

---
+++ components/zimAccessor/win/xpidl.sh
@@ -0,0 +1,4 @@
+../../../dependences/win/xulrunner-sdk/bin/xpidl.exe -m header -I../../../dependences/win/xulrunner-sdk/idl ../IZimaccessor.idl
+../../../dependences/win/xulrunner-sdk/bin/xpidl.exe -m typelib -I../../../dependences/win/xulrunner-sdk/idl ../IZimaccessor.idl
+mv IZimaccessor.* ../
+mv ../IZimAccessor.xpt ../zimAccessor.xpt
