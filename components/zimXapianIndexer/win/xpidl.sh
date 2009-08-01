1a988771292fcd9f6c08cf228269a91233de6be8
kelson42 <kelson42@users.sourceforge.net>
2009-08-01 09:23:44 +0000

    stub of the windows compilation scripts

---
+++ components/zimXapianIndexer/win/xpidl.sh
@@ -0,0 +1,3 @@
+../../../dependences/win/xulrunner-sdk/bin/xpidl.exe -m header -I../../../dependences/win/xulrunner-sdk/idl ../IZimxapianindexer.idl
+../../../dependences/win/xulrunner-sdk/bin/xpidl.exe -m typelib -I../../../dependences/win/xulrunner-sdk/idl ../IZimxapianindexer.idl
+mv IZimxapianindexer.* ../
