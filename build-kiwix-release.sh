ab6750a5ec59ed6f4b4c7fd6f3aeb7a592217e38
kelson42 <kelson42@users.sourceforge.net>
2009-08-13 19:00:49 +0000

    + factorisation of the code + use now xulrunner-1.9.0.13

--- build-kiwix-release.sh
+++ build-kiwix-release.sh
@@ -1,4 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+
+function getFirefoxLocalization {
+    CODE=$1
+    rm firefox-3.5.tar.bz2
+    wget -c http://releases.mozilla.org/pub/mozilla.org/firefox/releases/3.5/linux-i686/$CODE/firefox-3.5.tar.bz2
+    rm -rf ./firefox
+    tar -xvjf firefox-3.5.tar.bz2
+    cp ./firefox/chrome/$CODE.* ./kiwix/xulrunner/chrome/
+
+} 
 
 # Go the the /tmp directory
 cd /tmp
@@ -28,58 +38,23 @@ for i in `find ./kiwix -name ".svn"` ; do rm -rf $i ; done
 mv ./kiwix/chrome/content/main/js/logger_rlz.js ./kiwix/chrome/content/main/js/logger.js
 
 # Download and copy xulrunner
-wget http://releases.mozilla.org/pub/mozilla.org/xulrunner/releases/1.9.0.11/runtimes/xulrunner-1.9.0.11.en-US.linux-i686.tar.bz2
-tar -xvjf xulrunner-1.9.0.11.en-US.linux-i686.tar.bz2
+wget http://releases.mozilla.org/pub/mozilla.org/xulrunner/releases/1.9.0.13/runtimes/xulrunner-1.9.0.13.en-US.linux-i686.tar.bz2
+tar -xvjf xulrunner-1.9.0.13.en-US.linux-i686.tar.bz2
 mv ./xulrunner/ ./kiwix/
 
 # Download the firefox en copy the locales JAR
-rm firefox-3.5.tar.bz2
-wget -c http://releases.mozilla.org/pub/mozilla.org/firefox/releases/3.5/linux-i686/it/firefox-3.5.tar.bz2
-rm -rf ./firefox
-tar -xvjf firefox-3.5.tar.bz2
-cp ./firefox/chrome/it.* ./kiwix/xulrunner/chrome/
+getFirefoxLocalization de
+getFirefoxLocalization it
+getFirefoxLocalization fr
+getFirefoxLocalization es-ES
+getFirefoxLocalization ar
+getFirefoxLocalization he
+getFirefoxLocalization fa
+getFirefoxLocalization zh-CN
 
-rm firefox-3.5.tar.bz2
-wget -c http://releases.mozilla.org/pub/mozilla.org/firefox/releases/3.5/linux-i686/fr/firefox-3.5.tar.bz2
-rm -rf ./firefox
-tar -xvjf firefox-3.5.tar.bz2
-cp ./firefox/chrome/fr.* ./kiwix/xulrunner/chrome/
-
-rm firefox-3.5.tar.bz2
-wget -c http://releases.mozilla.org/pub/mozilla.org/firefox/releases/3.5/linux-i686/es-ES/firefox-3.5.tar.bz2
-rm -rf ./firefox
-tar -xvjf firefox-3.5.tar.bz2
-cp ./firefox/chrome/es-ES.* ./kiwix/xulrunner/chrome/
-
-rm firefox-3.5.tar.bz2
-wget -c http://releases.mozilla.org/pub/mozilla.org/firefox/releases/3.5/linux-i686/de/firefox-3.5.tar.bz2
-rm -rf ./firefox
-tar -xvjf firefox-3.5.tar.bz2
-cp ./firefox/chrome/de.* ./kiwix/xulrunner/chrome/
-
-rm firefox-3.5.tar.bz2
-wget -c http://releases.mozilla.org/pub/mozilla.org/firefox/releases/3.5/linux-i686/ar/firefox-3.5.tar.bz2
-rm -rf ./firefox
-tar -xvjf firefox-3.5.tar.bz2
-cp ./firefox/chrome/ar.* ./kiwix/xulrunner/chrome/
-
-rm firefox-3.5.tar.bz2
-wget -c http://releases.mozilla.org/pub/mozilla.org/firefox/releases/3.5/linux-i686/he/firefox-3.5.tar.bz2
-rm -rf ./firefox
-tar -xvjf firefox-3.5.tar.bz2
-cp ./firefox/chrome/he.* ./kiwix/xulrunner/chrome/
-
-rm firefox-3.5.tar.bz2
-wget -c http://releases.mozilla.org/pub/mozilla.org/firefox/releases/3.5/linux-i686/fa/firefox-3.5.tar.bz2
-rm -rf ./firefox
-tar -xvjf firefox-3.5.tar.bz2
-cp ./firefox/chrome/fa.* ./kiwix/xulrunner/chrome/
-
-rm firefox-3.5.tar.bz2
-wget -c http://releases.mozilla.org/pub/mozilla.org/firefox/releases/3.5/linux-i686/zh-CN/firefox-3.5.tar.bz2
-rm -rf ./firefox
-tar -xvjf firefox-3.5.tar.bz2
-cp ./firefox/chrome/zh-CN.* ./kiwix/xulrunner/chrome/
+# xapian-compact
+mkdir ./kiwix/bin
+cp moulinkiwix/dependences/xapian*/bin/xapian-compact ./kiwix/bin
 
 # Tar & clean
 rm kiwix.tar.bz2
