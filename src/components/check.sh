b4abceaee5c5a49c83093f8cc40dcb6177e51686
kelson42 <kelson42@users.sourceforge.net>
2009-11-14 14:14:17 +0000

    + small imp. to detect if the given file is a dynamic lib.

--- src/components/check.sh
+++ src/components/check.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
-
-RUN_MOZILLA=`find /usr/lib/xulrunner* -name run-mozilla.sh`;
 LIBRARY=$1;
-$RUN_MOZILLA `which ldd` -r $LIBRARY | grep unresolved;
+LDD=`which ldd`
+
+# Check if the file is a dynamic library
+$LDD $LIBRARY > /dev/null
+IS_LIBRARY=$?
+if [ ! "$IS_LIBRARY" -eq "0" ]
+then
+    echo "$LIBRARY is not a dynamic library."
+    exit
+fi
+
+# Check if dependences are missing
+RUN_MOZILLA=`find /usr/lib/xulrunner* -name run-mozilla.sh`;
+$RUN_MOZILLA $LDD -r $LIBRARY | grep unresolved;
