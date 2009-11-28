dc1f2a97c8bc24618664773c1a3549d8e8fda552
kelson42 <kelson42@users.sourceforge.net>
2009-11-28 16:20:52 +0000

    + deb package stub files

---
+++ debian/kiwix.default.ex
@@ -0,0 +1,10 @@
+# Defaults for kiwix initscript
+# sourced by /etc/init.d/kiwix
+# installed at /etc/default/kiwix by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(kiwix):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="kiwix" command="/usr/bin/kiwix"
