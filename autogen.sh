33feb979641abd6e67651398d118dd61b4025433
kelson42 <kelson42@users.sourceforge.net>
2009-12-14 10:51:22 +0000

    + small bug fix

--- autogen.sh
+++ autogen.sh
@@ -1,14 +1,14 @@
 #! /bin/sh
 
+# Generate the aclocal.m4 file for automake, based on configure.in
+aclocal
+
 # Regenerate the files autoconf / automake
 libtoolize --force --automake
 
 # Remove old cache files
 rm -f config.cache
 rm -f config.log
-
-# Generate the aclocal.m4 file for automake, based on configure.in
-aclocal
 
 # Generate the configure script based on configure.in
 autoconf
