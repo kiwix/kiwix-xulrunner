693b390e3b1b81fbdae2d7fa1adfbaaf117be402
kelson42 <kelson42@users.sourceforge.net>
2009-12-17 22:05:56 +0000

    + use the indexing factorized code from src/common/kiwix

--- src/components/zimXapianIndexer/zimXapianIndexer.cpp
+++ src/components/zimXapianIndexer/zimXapianIndexer.cpp
@@ -12,29 +12,9 @@
 #include "nsIProperties.h"
 #include "nsDirectoryServiceDefs.h"
 
-#include <zim/file.h>
-#include <zim/article.h>
-#include <zim/fileiterator.h>
-
-#include <xapian.h>
-#include "xapian/myhtmlparse.h"
-
-#include <unaccent.h>
+#include <kiwix/indexer.h>
 
 using namespace std;
-
-/* Count word */
-unsigned int countWords(const string &text) {
-  unsigned int numWords = 1;
-  for(int i=0; i<text.size();) {
-    while(i<text.size() && text[i] != ' ') {
-      i++;
-    }
-    numWords++;
-    i++;
-  }
-  return numWords;
-}
 
 class ZimXapianIndexer : public IZimXapianIndexer {
 
@@ -46,21 +26,7 @@ public:
 
 private:
   ~ZimXapianIndexer();
-
-protected:
-  zim::File* zimFileHandler;
-  zim::size_type firstArticleOffset;
-  zim::size_type lastArticleOffset;
-  zim::size_type currentArticleOffset;
-  zim::Article currentArticle;
-
-  unsigned int articleCount;
-  float stepSize;
-  
-  Xapian::WritableDatabase writableDatabase;
-  Xapian::Stem stemmer;
-  Xapian::TermGenerator indexer;
-  MyHtmlParser htmlParser;
+  kiwix::Indexer* indexer;
 };
 
 /* Implementation file */
@@ -68,18 +34,14 @@ NS_IMPL_ISUPPORTS1(ZimXapianIndexer, IZimXapianIndexer)
 
 /* Constructor */
 ZimXapianIndexer::ZimXapianIndexer() 
-: zimFileHandler(NULL), 
-  stemmer(Xapian::Stem("english")),
-  articleCount(0), 
-  stepSize(0) {
+: indexer(NULL) {
 }
 
 /* Destructor */
 ZimXapianIndexer::~ZimXapianIndexer() {
-  if (this->zimFileHandler != NULL) {
-    delete this->zimFileHandler;
+  if (this->indexer != NULL) {
+    delete this->indexer;
   }
-  //this->writableDatabase.~WritableDatabase();
 }
 
 /* Start indexing */
@@ -92,14 +54,12 @@ NS_IMETHODIMP ZimXapianIndexer::StartIndexing(const nsACString &zimFilePath,
   const char *cXapianDirectoryPath;
   NS_CStringGetData(xapianDirectoryPath, &cXapianDirectoryPath);
 
-  /* Open the ZIM file */
+  /* Create the indexer */
   try {    
-    this->zimFileHandler = new zim::File(cZimFilePath);
-
-    if (this->zimFileHandler != NULL) {
-      this->firstArticleOffset = this->zimFileHandler->getNamespaceBeginOffset('A');
-      this->lastArticleOffset = this->zimFileHandler->getNamespaceEndOffset('A');
-      this->currentArticleOffset = this->firstArticleOffset;
+    this->indexer = new kiwix::Indexer(cZimFilePath, cXapianDirectoryPath);
+    if (this->indexer != NULL) {
+      this->indexer->startIndexing();
+      *retVal = PR_TRUE;
     } else {
       *retVal = PR_FALSE;
     }
@@ -107,110 +67,23 @@ NS_IMETHODIMP ZimXapianIndexer::StartIndexing(const nsACString &zimFilePath,
     *retVal = PR_FALSE;
   }
 
-  /* Open the Xapian directory */
-  try {
-    this->writableDatabase = Xapian::WritableDatabase(cXapianDirectoryPath, 
-						      Xapian::DB_CREATE_OR_OVERWRITE);
-  } catch (...) {
-    *retVal = PR_FALSE;
-  }
-
-  /* Compute few things */
-  this->articleCount = this->zimFileHandler->getNamespaceCount('A');
-  this->stepSize = (float)this->articleCount / (float)100;
-
   return NS_OK;
-
 }
 
 /* Index next percent */
 NS_IMETHODIMP ZimXapianIndexer::IndexNextPercent(PRBool *retVal) {
-  *retVal = PR_TRUE;
-  float thresholdOffset = this->currentArticleOffset + this->stepSize;
-  size_t found;
-
-  while(this->currentArticleOffset < thresholdOffset && 
-	this->currentArticleOffset < this->lastArticleOffset) {
-
-    /* get next non redirect article */
-    do {
-      currentArticle = this->zimFileHandler->getArticle(this->currentArticleOffset);
-    } while (this->currentArticleOffset++ &&
-	     currentArticle.isRedirect() && 
-	     this->currentArticleOffset != this->lastArticleOffset);
-
-    if (!currentArticle.isRedirect()) {
-      
-      /* Index the content */
-      this->htmlParser.reset();
-      
-      try {
-		string content (currentArticle.getData().data(), currentArticle.getData().size());
-		this->htmlParser.parse_html(content, "UTF-8", true);
-      } catch(...) {
-      }
-      
-      /* if content does not have the noindex meta tag */
-      found=this->htmlParser.dump.find("NOINDEX");
-      
-      if (found == string::npos) {
-
-	/* Set the stemmer */
-	/* TODO, autodetect the language */
-	//indexer.set_stemmer(stemmer);
-	
-	/* Put the data in the document */
-	Xapian::Document document;
-	document.add_value(0, this->htmlParser.title);
-	document.set_data(currentArticle.getUrl().getValue().c_str());
-	indexer.set_document(document);
-	
-	/* Debug output */
-	std::cout << "Indexing " << currentArticle.getUrl().getValue() << "..." << std::endl;
-	
-	/* Index the title */
-	if (!this->htmlParser.title.empty()) {
-	  indexer.index_text_without_positions(removeAccents(this->htmlParser.title.c_str()), 
-					       ((this->htmlParser.dump.size() / 100) + 1) / 
-					       countWords(this->htmlParser.title) );
-	}
-	
-	/* Index the keywords */
-	if (!this->htmlParser.keywords.empty()) {
-	  indexer.index_text_without_positions(removeAccents(this->htmlParser.keywords.c_str()), 3);
-	}
-	
-	/* Index the content */
-	if (!this->htmlParser.dump.empty()) {
-	  indexer.index_text_without_positions(removeAccents(this->htmlParser.dump.c_str()));
-	}
-	
-	/* add to the database */
-	this->writableDatabase.add_document(document);
-      }
-    }
-  }
-
-  /* Write Xapian DB to the disk */
-  this->writableDatabase.flush();
-
-  /* increment the offset and set returned value */
-  if (this->currentArticleOffset < this->lastArticleOffset) {
-    this->currentArticleOffset++;
+  if (this->indexer->indexNextPercent()) {
     *retVal = PR_TRUE;
   } else {
-    this->currentArticleOffset = this->firstArticleOffset;
-	this->writableDatabase.~WritableDatabase();
     *retVal = PR_FALSE;
   }
-
+  
   return NS_OK;
 }
 
 /* Stop indexing. TODO: using it crashs the soft under windows. Have to do it in indexNextPercent() */
 NS_IMETHODIMP ZimXapianIndexer::StopIndexing(PRBool *retVal) {
   *retVal = PR_TRUE;
-  //this->writableDatabase.~WritableDatabase();
   return NS_OK;
 }
 
