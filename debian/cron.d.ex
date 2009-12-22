37cfa7f14447967000f6b1b482ec8cd14abf6243
kelson42 <kelson42@users.sourceforge.net>
2009-12-22 12:34:27 +0000

    + removed... not used

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the kiwix package
-#
-0 4	* * *	root	[ -x /usr/bin/kiwix_maintenance ] && /usr/bin/kiwix_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(kiwix):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="kiwix" command="/usr/bin/kiwix"
