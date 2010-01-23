6b7c71b69dba8d4c3d1e0b186660b1949041ac6e
kelson42 <kelson42@users.sourceforge.net>
2010-01-23 12:42:08 +0000

    + factorisation of the code, creation of the kiwix::Searcher class

--- src/components/xapianAccessor/xapianAccessor.cpp
+++ src/components/xapianAccessor/xapianAccessor.cpp
@@ -1,10 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string>
-#include <xapian.h>
-
-#include <unaccent.h>
-
 #include "xpcom-config.h"
 #include "nsIGenericFactory.h"
 #include "nsXPCOM.h"
@@ -17,16 +10,8 @@
 #include "nsDirectoryServiceDefs.h"
 #include "IXapianAccessor.h"
 
-#include "splitString.h"
-
-using namespace std;
-
-struct Result
-{
-  string url;
-  string title;
-  int score;
-}; 
+#include "kiwix/searcher.h"
+#include <string>
 
 class XapianAccessor : public IXapianAccessor {
 
@@ -38,34 +23,34 @@ public:
 
 private:
   ~XapianAccessor();
+  kiwix::Searcher *searcher;
 
-  Xapian::Database readableDatabase;
-  Xapian::Stem stemmer;
-  std::vector<Result> results;
-  std::vector<Result>::iterator resultOffset;
 };
 
 /* Implementation file */
 NS_IMPL_ISUPPORTS1(XapianAccessor, IXapianAccessor)
 
 /* Constructor */
-XapianAccessor::XapianAccessor() {
-  stemmer = Xapian::Stem("english");
+XapianAccessor::XapianAccessor() :
+searcher(NULL) {
 }
 
 /* Destructor */
 XapianAccessor::~XapianAccessor() {
+  if (this->searcher != NULL) {
+    delete this->searcher;
+  }
 }
 
 /* Open Xapian readable database */
 NS_IMETHODIMP XapianAccessor::OpenReadableDatabase(const nsACString &directory, PRBool *retVal) {
   *retVal = PR_TRUE;
-
+  
   const char *directoryPath;
   NS_CStringGetData(directory, &directoryPath);
-
+  
   try {
-    this->readableDatabase = Xapian::Database(directoryPath);
+    this->searcher = new kiwix::Searcher(directoryPath);
   } catch (...) {
     *retVal = PR_FALSE;
   }
@@ -81,48 +66,10 @@ NS_IMETHODIMP XapianAccessor::CloseReadableDatabase(PRBool *retVal) {
 
 /* Search strings in the database */
 NS_IMETHODIMP XapianAccessor::Search(const nsACString &search, PRUint32 resultsCount, PRBool *retVal) {
-  /* Reset the results */
-  this->results.clear();
-  this->resultOffset = this->results.begin();
-
-  /* Create the enquire object */
-  Xapian::Enquire enquire(this->readableDatabase);
-
-  /* Create the query term vector */
   const char *csearch;
   NS_CStringGetData(search, &csearch, NULL);
-  std::vector<std::string> queryTerms = split(removeAccents(csearch), " #@%$0/\\_-*()[]{},;:");
-  
-  /* Create query object */
-  Xapian::Query query(Xapian::Query::OP_OR, queryTerms.begin(), queryTerms.end());
 
-  /* Set the query in the enquire object */
-  enquire.set_query(query);
-
-  cout << "Performing query `" <<
-         query.get_description() << "'" << endl;
-  
-  /* Get the results */
-  Xapian::MSet matches = enquire.get_mset(0, resultsCount);
-
-  Xapian::MSetIterator i;
-  for (i = matches.begin(); i != matches.end(); ++i) {
-    Xapian::Document doc = i.get_document();
-
-    Result result;
-    result.url = doc.get_data();
-    result.title = doc.get_value(0);
-    result.score = i.get_percent();
-
-    this->results.push_back(result);
-
-    cout << "Document ID " << *i << "   \t";
-    cout << i.get_percent() << "% ";
-    cout << "\t[" << doc.get_data() << "] - " << doc.get_value(0) << endl;
-  }
-
-  /* Set the cursor to the begining */
-  this->resultOffset = this->results.begin();
+  this->searcher->search(csearch, resultsCount);
 
   *retVal = PR_TRUE;
   return NS_OK;
@@ -130,8 +77,7 @@ NS_IMETHODIMP XapianAccessor::Search(const nsACString &search, PRUint32 resultsC
 
 /* Reset the results */
 NS_IMETHODIMP XapianAccessor::Reset(PRBool *retVal) {
-  this->results.clear();
-  this->resultOffset = this->results.begin();
+  this->searcher->reset();
   *retVal = PR_TRUE;
   return NS_OK;
 }
@@ -139,22 +85,23 @@ NS_IMETHODIMP XapianAccessor::Reset(PRBool *retVal) {
 /* Get next result */
 NS_IMETHODIMP XapianAccessor::GetNextResult(nsACString &url, nsACString &title, PRUint32 *score, PRBool *retVal) {
   *retVal = PR_FALSE;
+  
+  std::string urlStr;
+  std::string titleStr;
+  unsigned int scoreInt;
 
-  if (this->resultOffset != this->results.end()) {
+  if (this->searcher->getNextResult(urlStr, titleStr, scoreInt)) {
 
     /* url */
-    url = nsDependentCString(this->resultOffset->url.c_str(), 
-			       this->resultOffset->url.length());
+    url = nsDependentCString(urlStr.c_str(), 
+			     urlStr.length());
 
     /* title */
-    title = nsDependentCString(this->resultOffset->title.c_str(), 
-			       this->resultOffset->title.length());
+    title = nsDependentCString(titleStr.c_str(), 
+			       titleStr.length());
 
     /* score */
-    *score =  this->resultOffset->score;
-
-    /* increment the cursor for the next call */
-    this->resultOffset++;
+    *score = scoreInt;
 
     *retVal = PR_TRUE;
   }
