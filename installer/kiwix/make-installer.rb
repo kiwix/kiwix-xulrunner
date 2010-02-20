cbbe1e600bc3e52b780d017399559f688ba1bf00
wilfredor <wilfredor@users.sourceforge.net>
2010-02-20 22:25:21 +0000

    new automatized script

--- installer/kiwix/make-installer.rb
+++ installer/kiwix/make-installer.rb
@@ -1,5 +1,4 @@
 #!/usr/bin/ruby
-
 =begin
 	This script is a wizard to build a list of files.
 	This list of files will be used to build an NSIS installer
@@ -9,24 +8,12 @@
 	wilfredor <wilfredor@kiwix.org>
 =end
 require 'ftools'
-
-def usage(exit_code)
-	puts "Usage:\t#{$0} [switches]
-        [#{$0} -e SOURCE_PATH COPY_SOURCE_PATH COPY_DEST_PATH LOG_FILE>]
-
-	-help		Displays this help message
-	-defaults	Uses default values and run
-	Else, runs through a wizard dialog.
-	"
-	exit(exit_code)
+def usage()
+	puts "\n ruby make-installer.rb --path=<path_to_the_dvd_file_tree>\n"
+	exit()
 end
 
-# default Path values
-# this path is necesary for make the
 # directory tree used in the nsi installer
-# place here the main kiwix application folder
-$source_path = "../kiwix/"
-$copy_source_path = "..\\kiwix"
 $copy_dest_path = "$INSTDIR"
 # file generated for build the install and unistall
 # section in the nsi file
@@ -39,74 +26,13 @@ $dirs = []
 $files = []
 $formats = []
 
-# welcome and help start message
-def welcome
-	puts ".-------------------------------------------------------------.\n"
-	puts "|   This script will help you generate a                      |\n"
-	puts "|   nsi source and a binary installer file.                   |\n"
-	puts "|   The output is file called 'kiwix-setup.nsi'               |\n"	
-	puts "|   and a installer bin called 'kiwix-setup.exe'              |\n"
-	puts "'-------------------------------------------------------------'\n\n"	
-	source_path()
-end
-
-# retrieves source path.
-def source_path
-	puts "Please, enter your kiwix's distribution path.\nDefault distribution Path: [#{$source_path}]: "
-	
-	a = String.new $stdin.gets
-	$source_path = a.chomp unless a.chomp.empty?
-	puts "You choosed: #{$source_path}\n"
-	path_position()
-end
-
-def path_position
-	puts "Choose the relative path to your installation files \n"
-	puts "from the _final installer_ on your _final media_. \n"
-	puts "Usually, your installer resides in cdrom/installer \n"
-	puts "and your files in cdrom/kiwix so path is `../kiwix`.\n\n"
-	puts "Path to files (use windows separators. ie: dir\otherdir) [#{$copy_source_path}]: "
-	
-	a = String.new $stdin.gets
-	$copy_source_path = a.chomp unless a.chomp.empty?
-	
-	puts "\nYou choosed: #{$copy_source_path}\n\n"
-	dest_path()
-end
-
-def dest_path
-	puts "Choose the _final_ destination path of the files.\n"
-	puts "Default is using a user-selected installation path stored in $INSTDIR.\n"
-	puts "Final Installation Path [#{$copy_dest_path}]: "
-	
-	a = String.new $stdin.gets
-	$copy_dest_path = a.chomp unless a.chomp.empty?
-	install_log()
-end
-
-def install_log
-	puts "Choose where to write the list. You can use special value STDOUT.\n"
-	puts "Output nsi file [#{$nsi_output}]: "
-	
-	a = String.new $stdin.gets
-	$nsi_output = (a.match(/stdout/i)) ? $stdout : a.chomp unless a.chomp.empty?
-	process()
-end
-
-def bye
-	puts "Processing done.\n"
-	puts "source_path: #{$source_path} \n"
-	puts "copy_source_path: #{$copy_source_path} \n"
-	puts "log_file: #{$nsi_output} \n"
-	puts "Please, remember copy all the #{$nsi_output} content in the nsis file \n"
-end
 # make a nsis file buildind the file tree from the path
 def process
 	File.delete($nsi_output)if File::exists?($nsi_output)
 	$source_path = $source_path.chop if $source_path[$source_path.length - 1, 1] == "/"
 	# make a new output file
 	kiwixnsi = ($nsi_output.class == IO) ? $nsi_output : File.new($nsi_output, "w")
-	recurs_display($source_path)
+	recurs_display($copy_source_path)
 	# writing directory tree for the nsi	
 	File.open($nsi_base, "r") do |infile|
 		while (line = infile.gets)
@@ -131,7 +57,7 @@ def process
 	system("makensis #{$nsi_output}")
 	#bye()
 end
-
+# build tree folder
 def recurs_display(dir)
 	Dir.foreach(dir) do |f|
 		next if [".","..", "moulin-linux","xulrunner-linux","moulin-mac.app","installed"].include? f
@@ -149,16 +75,16 @@ def recurs_display(dir)
 	end
 end
 # test if advanced mode
-if (ARGV.length == 5 and ARGV[0] == "-e") # assuming expert mode: take care !
-    $source_path = ARGV[1]
-    $copy_source_path = ARGV[2]
-    $copy_dest_path = ARGV[3]
-    $nsi_output = ARGV[4]
-    process()
-elsif (ARGV.length == 1 and ARGV[0].match(/default/))
-	process()
-elsif (ARGV.length == 1 and ARGV[0].match(/help/))
-	usage(0)
+if ARGV[0].include?"--path="
+	# get path value
+	$copy_source_path = ARGV[0].gsub "--path=",""
+	$source_path = $copy_source_path
+	# directory exist
+	if File.directory? $copy_source_path
+		process()
+	else
+		puts "\n The directory \"#{$copy_source_path}\" not exist \n"
+	end	
 else
-	welcome() # Launch the wizard.
+	usage() # show help
 end
