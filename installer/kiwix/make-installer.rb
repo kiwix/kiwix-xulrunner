8ff10d1ccde7c1361140c8bef60e727a49e03dfc
wilfredor <wilfredor@users.sourceforge.net>
2010-02-22 00:37:27 +0000

    new script, It has changed in its entirety

--- installer/kiwix/make-installer.rb
+++ installer/kiwix/make-installer.rb
@@ -13,42 +13,44 @@ def usage()
 	puts "\n ruby make-installer.rb --path=<path_to_the_dvd_file_tree>\n"
 	exit()
 end
-
 # directory tree used in the nsi installer
 $copy_dest_path = "$INSTDIR"
 # file generated for build the install and unistall
 # section in the nsi file
 $nsi_base = "nsi-template.nsi"
 $nsi_output = "kiwix-setup.nsi"
-# not included in the nsi package
-$exceptions = [".","..", "moulin-linux","xulrunner-linux","moulin-mac.app","installed","bonus","autorun","autorun.inf","install"]
 $content = String.new
 # internal variables
-$dirs = []
 $files = []
-$formats = []
-
+$dirs = []
+# list files for build the installer
+$files_list = [
+   "/kiwix/{kiwix.exe,application.ini}",
+   "/data/*",				
+   "/kiwix/xulrunner/*",       
+   "/kiwix/chrome/*",          
+   "/kiwix/defaults/*",        
+   "/kiwix/components/*.{dll,xpt,js}" 
+]
 # make a nsis file buildind the file tree from the path
 def process
 	File.delete($nsi_output)if File::exists?($nsi_output)
 	$source_path = $source_path.chop if $source_path[$source_path.length - 1, 1] == "/"
 	# make a new output file
 	kiwixnsi = ($nsi_output.class == IO) ? $nsi_output : File.new($nsi_output, "w")
-	recurs_display($copy_source_path)
+	# build the tree
+	build_tree($copy_source_path)
 	# writing directory tree for the nsi	
 	File.open($nsi_base, "r") do |infile|
 		while (line = infile.gets)
 			kiwixnsi.puts "#{line}"
 			if "#{line}".include?"CreateDirectory \"$INSTDIR\"" then
-
 				# put the tree file on the instalation section nsi file
 				kiwixnsi.puts "; INSTALLATION PART \n"
 				kiwixnsi.puts "; Section Automatically generated make-installer.rb \n"
-				
 				$dirs.each do |d|
 					kiwixnsi.puts "\tCreateDirectory `#{d}`"
 				end
-				
 				$files.each do |f|
 					kiwixnsi.puts "\tCopyFiles `#{f[0]}` `#{f[1]}`"
 				end
@@ -58,27 +60,27 @@ def process
 	end  	  
 	kiwixnsi.close 
 	# compile the nsi file
-	system("makensis #{$nsi_output}")
+	# /V0 hide log
+	system("makensis /V0 #{$nsi_output}")
 end
 # build tree folder
-def recurs_display(dir)
-	Dir.foreach(dir) do |f|
-		next if $exceptions.include? f
-		fp	= dir+File::SEPARATOR+f
-		wd	= fp.slice($source_path.length, fp.length).gsub("/","\\")
-		wdd	= wd.slice(0, (wd.length - (f.length + 1)))
-		if (File.ftype(fp) == "directory")
-			$dirs << "#{$copy_dest_path}#{wd}"
-			recurs_display(fp)
+def build_tree(dvd_path)
+	$files_list.each do |dire| 
+		current_dir = dire.gsub("/"+File.basename(dire),"").gsub("/","\\")
+		if !File.directory? dvd_path + current_dir
+			puts "Warning!: Folder not found \"#{current_dir}\" in your path \"#{dvd_path}\""
 		else
-			$files << ["#{$copy_source_path}#{wd}", "#{$copy_dest_path}#{wdd}", "#{$copy_dest_path}#{wd}"]
-			
-			$formats << ["raw-format", "#{$copy_dest_path}#{wdd}\\format", "#{$copy_dest_path}#{wd}"] if fp =~ /datas.*format/
+			$dirs << $copy_dest_path + current_dir
+			Dir.glob(dvd_path+"#{dire}").each do |f|
+				fp	= dvd_path + f
+				wd	= fp.slice(dvd_path.length, fp.length).gsub("/","\\")
+				$files << ["#{wd}", "#{$copy_dest_path}#{current_dir}", "#{$copy_dest_path}#{wd}"]
+			end
 		end
 	end
 end
 # argument path
-if ARGV[0].nil?
+if ARGV[0].nil? 
 	usage() # show help
 else	
 	if ARGV[0].include?"--path="
@@ -89,7 +91,7 @@ else
 			$source_path = $copy_source_path
 			process() # run main procedure
 		else
-			puts "\n The directory \"#{$copy_source_path}\" not exist \n"
+			puts "\n The directory \"#{$copy_source_path}\" not found \n"
 		end	
 	else
 		usage() # show help
