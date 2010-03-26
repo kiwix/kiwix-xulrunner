7c2874d4f3487d5812f71ea446a1ffd2bc88a81e
wilfredor <wilfredor@users.sourceforge.net>
2010-03-26 04:33:00 +0000

    Change the code in almost its entirety. Paradigm Shift (Object Oriented Programming) thinking in a graphical environment future

--- installer/kiwix/make-installer.rb
+++ installer/kiwix/make-installer.rb
@@ -9,132 +9,166 @@
 =end
 
 require 'ftools'
-def usage()
+
+class Nsi_output 
+	attr_reader :arg
+	def initialize(arg,files_list)
+		# internal variables
+		@files = []
+		@dirs = []
+		
+		@files_list = files_list
+		@message =""
+		@arg = arg
+	end
+	
+	def message
+		@message
+	end
+	
+	def make()
+		if verify_dirs() then
+			File.delete(@arg['nsi_output'])if File::exists?(@arg['nsi_output'])
+			# make a new output file
+			kiwixnsi = (@arg['nsi_output'].class == IO) ? @arg['nsi_output'] : File.new(@arg['nsi_output'], "w")
+			# build the tree
+			build_tree(@arg['source_path'])
+			write_file(kiwixnsi)
+			# compile the nsi file
+			# -V0 hide log
+			system("makensis -V0 #{@arg['nsi_output']}")
+			success()
+		end
+	end
+	  
+	def write_file(kiwixnsi)
+		# writing directory tree for the nsi	
+		File.open(@arg['nsi_base'], "r") do |infile|
+			while (line = infile.gets)
+				kiwixnsi.puts "#{line}"
+				if "#{line}".include?"CreateDirectory \"#{@arg['copy_dest_path']}\"" then
+					# put the tree file on the instalation section nsi file
+					kiwixnsi.puts "; INSTALLATION PART \n"
+					kiwixnsi.puts "; Section Automatically generated \n"
+					@dirs.each do |d|
+						kiwixnsi.puts "\tCreateDirectory `#{d}`"
+					end
+					# if a portable packed installer
+					if @arg['allinone'] then
+						@files.each do |f|
+							path_full = @arg['source_path'].gsub("/","\\")+f[0].gsub("..","")
+							file_out = f[0].gsub("..","").gsub("\\"+File.basename(path_full),"")
+							kiwixnsi.puts "\tSetOutPath \"#{@arg['copy_dest_path']}#{file_out}\""
+							kiwixnsi.puts "\tFile `#{path_full}`"
+						end
+					else
+						# if is a installer for copy from a DVD
+						@files.each do |f|
+							kiwixnsi.puts "\tCopyFiles /SILENT `#{f[0]}` `#{f[1]}`"
+					end
+					end
+					kiwixnsi.puts "\n; EXTRACT STUB \n"				
+				end
+			end
+		end  	  
+		kiwixnsi.close 
+	end
+
+	# build tree folder
+	def build_tree(dvd_path)
+			@files_list.each do |dire| 
+				current_dir = dire.gsub("/"+File.basename(dire),"").gsub("/","\\")
+				@dirs << @arg['copy_dest_path'] + current_dir
+				Dir.glob(dvd_path+"#{dire}").each do |f|			
+					current = f.gsub(dvd_path,"").gsub("/","\\")
+					if File.directory? f
+						@dirs << @arg['copy_dest_path'] + current
+					else
+						wd = f.gsub(dvd_path,@arg['relative_path']).gsub("/","\\")
+						@files << ["#{wd}", "#{@arg['copy_dest_path']}#{current}", "#{@arg['copy_dest_path']}#{current}"]
+					end
+				end
+			end
+	end
+		
+	# final process
+	def success
+			# verify if the binary installer exist
+			if (File.exist?("#{@arg['bin_output']}"))
+				# copy the file in the DVD file hierarchy
+				system("cp #{@arg['bin_output']} \"#{@arg['install_dir']}\"")
+				@message =  "Done!. #{arg['bin_output']} is in #{arg['install_dir']}"				
+			else
+				# try make the installer and show all log messages
+				system("makensis #{@arg['nsi_output']}")
+			end
+	end
+	
+	def is_dir(dir)
+		if !File.directory? @arg[dir] then
+			@message = "\n Error. Directory \"#{@arg[dir]}\" not found \n"
+			return false
+		else
+			return true
+		end
+	end
+	
+	def verify_dirs()
+		# directory exist
+		return is_dir('source_path') && is_dir('install_dir')
+	end
+	
+end
+
+def help()
 	puts "\n ruby make-installer.rb --path=<path_to_the_dvd_file_tree> <--allinone>\n"
 	puts "\n allinone: Build a installer packed portable"
 	exit()
 end
-# directory tree used in the nsi installer
-$copy_dest_path = "$INSTDIR"
 
-# relative path for the installer 
-# really in DVD_PATH/kiwix/install/win/
-$relative_path = ".."
-# template for build the final nsi
-$nsi_base = "kiwix-install.nsi.tmpl"
-# file generated for build the install and unistall
-$nsi_output = "kiwix-install.nsi"
-
-# internal variables
-$files = []
-$dirs = []
-
-# list files for build the installer
-# /**/** is a recursive search in the folder
-$files_list = [
-   "/kiwix/{kiwix.exe,application.ini}",
-   "/data/**/**",				
-   "/kiwix/xulrunner/**/**",       
-   "/kiwix/chrome/**/**",          
-   "/kiwix/defaults/**/**",        
-   "/kiwix/components/*.{dll,xpt,js}" 
-]
-
-# make a nsis file buildind the file tree from the path
-def process
-	File.delete($nsi_output)if File::exists?($nsi_output)
-	$source_path = $source_path.chop if $source_path[$source_path.length - 1, 1] == "/"
-	# make a new output file
-	kiwixnsi = ($nsi_output.class == IO) ? $nsi_output : File.new($nsi_output, "w")
-	# build the tree
-	build_tree($copy_source_path)
-	# writing directory tree for the nsi	
-	File.open($nsi_base, "r") do |infile|
-		while (line = infile.gets)
-			kiwixnsi.puts "#{line}"
-			if "#{line}".include?"CreateDirectory \"$INSTDIR\"" then
-				# put the tree file on the instalation section nsi file
-				kiwixnsi.puts "; INSTALLATION PART \n"
-				kiwixnsi.puts "; Section Automatically generated \n"
-				$dirs.each do |d|
-					kiwixnsi.puts "\tCreateDirectory `#{d}`"
-				end
-				# if a portable packed installer
-				if $allinone then
-					$files.each do |f|
-						path_full = $copy_source_path.gsub("/","\\")+f[0].gsub("..","")
-						file_out = f[0].gsub("..","").gsub("\\"+File.basename(path_full),"")
-						kiwixnsi.puts "\tSetOutPath \"$INSTDIR#{file_out}\""
-						kiwixnsi.puts "\tFile `#{path_full}`"
-					end
-				else
-					# if is a installer for copy from a DVD
-					$files.each do |f|
-					kiwixnsi.puts "\tCopyFiles /SILENT `#{f[0]}` `#{f[1]}`"
-				end
-				end
-				kiwixnsi.puts "\n; EXTRACT STUB \n"				
-			end
-		end
-	end  	  
-	kiwixnsi.close 
-	# compile the nsi file
-	# -V0 hide log
-	
-	system("makensis -V0 #{$nsi_output}")
-end
-
-# build tree folder
-def build_tree(dvd_path)
-	$files_list.each do |dire| 
-		current_dir = dire.gsub("/"+File.basename(dire),"").gsub("/","\\")
-		$dirs << $copy_dest_path + current_dir
-		Dir.glob(dvd_path+"#{dire}").each do |f|			
-			current = f.gsub(dvd_path,"").gsub("/","\\")
-			if File.directory? f
-				$dirs << $copy_dest_path + current
-			else
-				wd = f.gsub(dvd_path,$relative_path).gsub("/","\\")
-				$files << ["#{wd}", "#{$copy_dest_path}#{current}", "#{$copy_dest_path}#{current}"]
-			end
-		end
+#get argument value
+def get_argument(argument)
+	if (argument == nil) then
+		return false
+	elsif argument.include? "=" then		
+		return (argument.split "=")[1]				
+	else
+		return true		
 	end
 end
 
-# final process
-def finalprocess
-	# verify if the binary installer exist
-	if (File.exist?("#{$bin_output}"))
-		# copy the file in the DVD file hierarchy
-		system("cp #{$bin_output} \"#{$install_dir}\"")
-		puts "Done!. #{$bin_output} is in #{$install_dir}"		
-	else
-		# try make the installer and show all log messages
-		system("makensis #{$nsi_output}")
+begin
+	$source_path = get_argument(ARGV[0])
+	if !$source_path then
+		help()
 	end
-end
-# argument path validation
-if (ARGV[0] == nil)? false : ((ARGV[0].include?"--path=")? true : false)
-	$allinone = (ARGV[1] == nil)? false : ((ARGV[1].include?"--allinone")? true : false)
-	# get path value
-	$copy_source_path = ARGV[0].gsub "--path=",""
-	# output dir and binary installer file
-	$bin_output = $nsi_output.gsub(".nsi",".exe")
-	$install_dir = "#{$copy_source_path}install/"
-	# directory exist
-	if File.directory? $copy_source_path
-		if File.directory? $install_dir
-			$source_path = $copy_source_path
-			process() # run main procedure
-			finalprocess()
-		else
-			puts "\n Error. Directory \"#{$install_dir}\" not found \n"
-		end
-	else
-		puts "\n Error. Directory \"#{$copy_source_path}\" not found \n"
-	end	
-
-else
-	usage() # show help
-end
+	# list files for build the installer
+	# /**/** is a recursive search in the folder
+	files_list = [
+		   "/kiwix/{kiwix.exe,application.ini}",
+		   "/data/**/**",				
+		   "/kiwix/xulrunner/**/**",       
+		   "/kiwix/chrome/**/**",          
+		   "/kiwix/defaults/**/**",        
+		   "/kiwix/components/*.{dll,xpt,js}" 
+		]
+	arg = Hash.new
+	arg = {
+		"allinone"       => get_argument(ARGV[1]),
+		# get path value
+		"source_path"    => $source_path,
+		# output nsi,dir and binary installer file
+		"nsi_output"     => "kiwix-install.nsi",
+		"bin_output"     => "kiwix-install.exe",
+		"nsi_base"       => "kiwix-install.nsi.tmpl",
+		"install_dir"    => "#{$source_path}install/",
+		"copy_dest_path" => "$INSTDIR",
+		"relative_path"  => ".."
+	}
+	# verify arguments need
 	
+	out = Nsi_output.new(arg,files_list)
+	out.make()
+	
+	puts out.message
+end
