5bf5352780c2f4c875eb7f79609340c696c8e012
kelson42 <kelson42@users.sourceforge.net>
2010-08-03 11:05:24 +0000

    + stub

---
+++ src/components/zimIndexer/zimIndexer.cpp
@@ -0,0 +1,80 @@
+#include "xpcom-config.h"
+#include "nsIGenericFactory.h"
+#include "IZimXapianIndexer.h"
+
+#include "nsXPCOM.h"
+#include "nsEmbedString.h"
+#include "nsIURI.h"
+
+#include "nsIServiceManager.h"
+#include "nsIFile.h"
+#include "nsCOMPtr.h"
+#include "nsIProperties.h"
+#include "nsDirectoryServiceDefs.h"
+
+#include <kiwix/indexer.h>
+
+using namespace std;
+
+class ZimIndexer : public IZimIndexer {
+
+public:
+  NS_DECL_ISUPPORTS
+  NS_DECL_IZIMINDEXER
+  
+  ZimIndexer();
+
+private:
+  ~ZimIndexer();
+  kiwix::Indexer* indexer;
+};
+
+/* Implementation file */
+NS_IMPL_ISUPPORTS1(ZimIndexer, IZimIndexer)
+
+/* Constructor */
+ZimIndexer::ZimIndexer() 
+: indexer(NULL) {
+}
+
+/* Destructor */
+ZimIndexer::~ZimIndexer() {
+  if (this->indexer != NULL) {
+    delete this->indexer;
+  }
+}
+
+/* Start indexing */
+NS_IMETHODIMP ZimIndexer::StartIndexing(const nsACString &zimPath, 
+					const nsACString &indexPath,
+					nsACString &backend, 
+					PRBool *retVal) {
+  *retVal = PR_FALSE;
+  return NS_OK;
+}
+
+/* Index next percent */
+NS_IMETHODIMP ZimXapianIndexer::GetStatus(PRUint32 *count, PRBool *retVal) {
+  *retVal = PR_FALSE;
+  return NS_OK;
+}
+
+/* Stop indexing. TODO: using it crashs the soft under windows. Have to do it in indexNextPercent() */
+NS_IMETHODIMP ZimXapianIndexer::StopIndexing(PRBool *retVal) {
+  *retVal = PR_TRUE;
+  return NS_OK;
+}
+
+NS_GENERIC_FACTORY_CONSTRUCTOR(ZimXapianIndexer)
+
+static const nsModuleComponentInfo components[] =
+{
+   { "zimIndexer",
+     IZIMINDEXER_IID,
+     "@kiwix.org/zimIndexer",
+     ZimIndexerConstructor
+   }
+};
+
+NS_IMPL_NSGETMODULE(nsZimIndexer, components)
+
