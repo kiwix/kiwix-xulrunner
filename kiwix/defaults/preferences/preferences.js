104d0ceef6dd883d03699d0af4c4485cf7e64e05
kelson42 <kelson42@users.sourceforge.net>
2010-10-30 11:47:45 +0000

    + propose a download dialog if the content mime-type is unknown

--- kiwix/defaults/preferences/preferences.js
+++ kiwix/defaults/preferences/preferences.js
@@ -46,3 +46,21 @@ pref("browser.download.manager.showWhenStarting", false);
 
 /* History */
 pref("browser.sessionhistory.max", -1);
+
+/* Download dialog if Mimetype is not supported */
+pref("browser.download.useDownloadDir", false);
+pref("browser.helperApps.deleteTempFileOnExit", true);
+pref("browser.download.folderList", 0);
+pref("browser.download.manager.showAlertOnComplete", true);
+pref("browser.download.manager.showAlertInterval", 2000);
+pref("browser.download.manager.retention", 2);
+pref("browser.download.manager.showWhenStarting", true);
+pref("browser.download.manager.useWindow", true);
+pref("browser.download.manager.closeWhenDone", true);
+pref("browser.download.manager.openDelay", 0);
+pref("browser.download.manager.focusWhenStarting", false);
+pref("browser.download.manager.flashCount", 2);
+pref("alerts.slideIncrement", 1);
+pref("alerts.slideIncrementTime", 10);
+pref("alerts.totalOpenTime", 4000);
+pref("alerts.height", 50);
