6e0ab63edb583eb1bf76efb72098e4397e681257
kelson42 <kelson42@users.sourceforge.net>
2010-12-04 17:47:06 +0000

    + fix a bug if the kiwix was on the root of a USB key (data was not installed) + avoid a duplicate copy of "data"

--- installer/make-installer.rb
+++ installer/make-installer.rb
@@ -64,7 +64,6 @@ class Nsi_output
                        @files.each do |f|
                                $result += "\tCopyFiles /SILENT `#{f[0]}` `#{f[1]}` `#{f[2]}`\n"
                        end
-		       $result += "\tCopyFiles /SILENT `data\\*` `$INSTDIR\\data\\`\n"
                end
 
                # return tree section for put in nsi template
