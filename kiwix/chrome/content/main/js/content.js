6ca7f6508ceec07826e9348a39c5b64ec5aec58d
kelson42 <kelson42@users.sourceforge.net>
2011-02-26 12:06:07 +0000

    + add function getDownloadStatus() to content.js

--- kiwix/chrome/content/main/js/content.js
+++ kiwix/chrome/content/main/js/content.js
@@ -11,16 +11,25 @@ function loadBinaryResource(url) {
 }
 
 function startDownloader() {
-    var binary = Components.classes["@mozilla.org/file/local;1"]
-	.createInstance(Components.interfaces.nsILocalFile);
-    binary.initWithPath("/usr/bin/aria2c");
+    var backgroundTask = {
+	run: function() {
+	    var binary = Components.classes["@mozilla.org/file/local;1"]
+	    .createInstance(Components.interfaces.nsILocalFile);
+	    binary.initWithPath("/usr/bin/aria2c");
+	    
+	    aria2Process = Components.classes["@mozilla.org/process/util;1"]
+	    .createInstance(Components.interfaces.nsIProcess);
+	    aria2Process.init(binary);
+	    
+	    var args = [ "--enable-xml-rpc", "--log=" + getDownloaderLogPath() ];
+	    aria2Process.run(false, args, args.length);
+	}
+    }
 
-    aria2Process = Components.classes["@mozilla.org/process/util;1"]
-	.createInstance(Components.interfaces.nsIProcess);
-    aria2Process.init(binary);
-
-    var args = [ "--enable-xml-rpc", "--log=" + getDownloaderLogPath() ];
-    aria2Process.run(false, args, args.length);
+    var thread = Components.classes["@mozilla.org/thread-manager;1"]
+	.getService(Components.interfaces.nsIThreadManager)
+	.newThread(0);
+    thread.dispatch(backgroundTask, thread.DISPATCH_NORMAL);
 }
 
 function stopDownloader() {
@@ -29,17 +38,33 @@ function stopDownloader() {
 }
 
 function startDownload(url) {
-    var torrentContent = loadBinaryResource(url);
-    var param = new xmlrpcval(torrentContent, "base64");
-    var msg = new xmlrpcmsg("aria2.addTorrent", [ param ]);
-    var response = aria2Client.send(msg);
-    return response.val.scalarVal();
+    var backgroundTask = {
+	run: function() {
+	    var torrentContent = loadBinaryResource(url);
+	    var param = new xmlrpcval(torrentContent, "base64");
+	    var msg = new xmlrpcmsg("aria2.addTorrent", [ param ]);
+	    var response = aria2Client.send(msg);
+	    response.val.scalarVal();
+	}
+    }
+
+    var thread = Components.classes["@mozilla.org/thread-manager;1"]
+	.getService(Components.interfaces.nsIThreadManager)
+	.newThread(0);
+    thread.dispatch(backgroundTask, thread.DISPATCH_NORMAL);
 }
 
 function stopDownload(index) {
     var param = new xmlrpcval(index, "base64");
     var msg = new xmlrpcmsg("aria2.remove", [ param ]);
     var response = aria2Client.send(msg);
+}
+
+function getDownloadStatus() {
+    var msg = new xmlrpcmsg("aria2.tellActive");
+    var response = aria2Client.send(msg);
+    var downloadStatus = response.val.arrayMem(0);
+    alert(downloadStatus.structMem('downloadSpeed').scalarVal());
 }
 
 /* Return the tmp directory path where the search index is build */
