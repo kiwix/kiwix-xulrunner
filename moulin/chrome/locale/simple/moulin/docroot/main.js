2ea435faa149174f8ae344af68acd16984ff6128
kelson42 <kelson42@users.sourceforge.net>
2011-09-15 16:19:19 +0000

    The 'moulin' directory is deprecated

--- moulin/chrome/locale/simple/moulin/docroot/main.js
+++
@@ -1,7 +0,0 @@
-function search() {
-    q = document.getElementById("search-query").value;
-    l = document.getElementById("lang").value;
-    p = document.getElementById("project").value;
-    d = document.getElementById("search-link");
-    d.setAttribute("href", "moulin-search://"+p+"/"+l+"/"+q);
-}
