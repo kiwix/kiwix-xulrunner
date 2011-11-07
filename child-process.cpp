303b051f7676fce48d6d3963fb34cf6693d64e20
kelson42 <kelson42@users.sourceforge.net>
2011-11-07 23:36:42 +0000

    test programs

---
+++ child-process.cpp
@@ -0,0 +1,34 @@
+/*                                                                                                                                                           * Copyright 2011 Emmanuel Engelhart <kelson@kiwix.org>                                                                                                      *                                                                                                                                                           * This program is free software; you can redistribute it and/or modify                                                                                      * it under the terms of the GNU  General Public License as published by                                                                                     * the Free Software Foundation; either version 3 of the License, or                                                                                         * any later version.                                                                                                                                        *                                                                                                                                                           * This program is distributed in the hope that it will be useful,                                                                                           * but WITHOUT ANY WARRANTY; without even the implied warranty of                                                                                            * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                                                                                              * GNU General Public License for more details.                                                                                                              *                                                                                                                                                           * You should have received a copy of the GNU General Public License                                                                                         * along with this program; if not, write to the Free Software                                                                                               * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,                                                                                                * MA 02110-1301, USA.                                                                                                                                       */
+
+#include <string>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+  if (argc<1) {
+    cerr << "Usage: child-process PPID" << endl;
+    return 1;
+  }
+
+  unsigned int PPID = atoi(argv[0]);
+  cout << "child-process: PPID is " << PPID << endl; 
+
+  string procPath = "/proc/" + string(argv[0]);
+  bool waiting = true;
+  do {
+    if (access(procPath.c_str(), F_OK) != -1) {
+      cout << "child-process: PPID " << argv[0] << " is running" << endl;
+      sleep(1);
+    } else {
+      cout << "child-process: PPID " << argv[0] << " is NOT running" << endl;
+      waiting = false;
+    }
+  } while (waiting);
+
+  cout << "child-process: exiting..." << endl; 
+  return 0;
+}
+
---
+++ parent-process.cpp
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2011 Emmanuel Engelhart <kelson@kiwix.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU  General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ */
+
+#include <string>
+#include <iostream>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+  unsigned int delay = 10;
+  string childBinaryPath="child-process";
+
+  /* Get PPID */
+  pid_t PID = getpid(); 
+  cout << "parent-process: PID is " << PID << endl;
+
+  /* Launch child-process */
+  char PIDStr[10];
+  sprintf(PIDStr, "%d", PID);
+  cout << "parent-process: launching child-process from path " << childBinaryPath << "..."<< endl;
+
+  PID = fork();
+  switch (PID) {
+  case -1:
+    cerr << "parent-process: Unable to fork" << endl;
+    return 1;
+    break;
+  case 0: /* This is the child process */
+    if (execl(childBinaryPath.c_str(), PIDStr, NULL) == -1) {
+      cerr << "parent-process: unable to start child-process from path " << childBinaryPath << endl;
+      return 1;
+    }
+    return 0;
+    break;
+  default:
+    cout << "parent-process: has forked successfuly" << endl;
+    cout << "parent-process: child-process PID is " << PID << endl;
+    break;
+  }
+
+  /* Start countdown */
+  cout << "parent-process: countdown started..." << endl;
+  do {
+    cout << "parent-process:" << delay << endl;
+    sleep(1);
+  } while (delay-- > 0);
+
+  /* Exit, child-process should also exit consequently */
+  cout << "parent-process: exiting..." << endl;
+
+  return 0;
+}
