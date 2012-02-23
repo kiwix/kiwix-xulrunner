c39e66f60f3bab233cd6f48a15ac7cb7d6e10af9
reg_ <reg_@users.sourceforge.net>
2012-02-23 15:27:17 +0000

    added alternative configure script with mechanism to switch
    configure.ac no longer exist.
    It has been moved to configure_orig.ac
    Another script (configure_alt.ac) has been added.
    autogen.sh now creates a symlink to the appropriate configure.ac.
    By default, it uses the _orig one.
    launch autogen.sh alt to use the new one or autogen.sh orig to restore the default one.
    Please DO NOT add configure.ac back to subversion repo.

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,24 @@
 #! /bin/sh
 
+# create symlink to configure.ac
+CONF_ALT=""
+if [ "$1" != "" ] ; then
+    CONF_ALT=$1
+fi
+
+# overwrite existing configuration
+if [ "${CONF_ALT}" = "alt" -o  "${CONF_ALT}" = "orig" ]; then
+    ln -sf "configure_${CONF_ALT}.ac" configure.ac
+
+fi
+
+# if no configure, create default
+if [ ! -f configure.ac ]; then
+    ln -s configure_orig.ac configure.ac
+fi
+
+ls -lh configure.ac
+
 # Generate the aclocal.m4 file for automake, based on configure.in
 aclocal
 
