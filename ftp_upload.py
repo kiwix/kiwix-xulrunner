d87129d1a7fa9828d67102948863ec8f20aca759
renaud gaudin <rgaudin@gmail.com>
2013-08-07 01:06:52 +0800

    added dest_name param

--- ftp_upload.py
+++ ftp_upload.py
@@ -12,11 +12,12 @@ from ftplib import FTP
 """ Kiwix nightly builds uploader
 
     Sends a build file to the nightly builds repository via FTP.
-    Usage: script.py password_module source_path dest_folder
+    Usage: script.py password_module source_path dest_folder dest_name
 
         password_module: path of a python file containing FTP_PASSWD variable
         source_path: path of file to transfer on local machine.
-        dest_folder: relative path on FTP server. Usually `latest/` """
+        dest_folder: relative path on FTP server. Usually `latest/`
+        dest_name: filename to give on FTP server """
 
 FTP_HOST = "download.kiwix.org"
 FTP_PORT = 21
@@ -30,12 +31,13 @@ def getpasswd(pathname):
 
 def main(argv):
 
-    if len(argv) != 3:
-        print("Usage:\t{0} passwd_module_path source_path destination_folder\n\n"
+    if len(argv) != 4:
+        print("Usage:\t{0} passwd_module_path source_path "
+              "destination_folder destination_name\n\n"
               "Missing arguments.".format(sys.argv[0]))
         sys.exit(1)
 
-    passwd_module_path, source_path, dest_folder = argv
+    passwd_module_path, source_path, dest_folder, dest_name = argv
     FTP_PASSWD = getpasswd(passwd_module_path)
     filename = os.path.basename(source_path)
 
@@ -52,8 +54,8 @@ def main(argv):
     print("Moving to directory {}".format(dest_folder))
     ftp.cwd(dest_folder)
 
-    print("Staring upload of {}".format(filename))
-    ftp.storbinary('STOR {}'.format(filename), open(source_path, 'rb'))
+    print("Staring upload of {} to {}".format(filename, dest_name))
+    ftp.storbinary('STOR {}'.format(dest_name), open(source_path, 'rb'))
 
     print("Transfer complete. Closing")
     ftp.quit()
