f4270662d3382bd8ebec0692cb79b78c1f334d0b
renaud gaudin <rgaudin@gmail.com>
2013-08-10 22:14:18 +0800

    ctpp2 as external lib on unix

--- autogen.sh
+++ autogen.sh
@@ -1,19 +1,17 @@
 #! /bin/sh
 
-if [ "`(uname -s) 2>/dev/null`" = "Darwin" ]
-then
-    LIBTOOLIZE=glibtoolize
-else
-    LIBTOOLIZE=libtoolize
-fi
-
 ls -lh configure.ac
 
 # Generate the aclocal.m4 file for automake, based on configure.in
 aclocal
 
 # Regenerate the files autoconf / automake
-$LIBTOOLIZE --force --automake
+if [ "`(uname -s) 2>/dev/null`" = "Darwin" ]
+then
+    glibtoolize --force
+else
+    libtoolize --force --automake
+fi
 
 # Remove old cache files
 rm -f config.cache
