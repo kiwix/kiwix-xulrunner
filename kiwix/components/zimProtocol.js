3556d11a641ba517453a3e506453f7d069307e35
kelson42 <kelson@kiwix.org>
2014-05-07 12:26:40 +0200

    + Allow to deal with relative urls

--- kiwix/components/zimProtocol.js
+++ kiwix/components/zimProtocol.js
@@ -23,29 +23,71 @@ ZimprotocolHandler.prototype = {
     allowPort: function(port, scheme) { return false; },
 
     newURI: function(spec, charset, baseURI) {
+	/* Functions to make an url absolute */
+	function parseURI(url) {
+	    var m = String(url).replace(/^\s+|\s+$/g, '').match(/^([^:\/?#]+:)?(\/\/(?:[^:@]*(?::[^:@]*)?@)?(([^:\/?#]*)(?::(\d*))?))?([^?#]*)(\?[^#]*)?(#[\s\S]*)?/);
+	    // authority = '//' + user + ':' + pass '@' + hostname + ':' port
+	    return (m ? {
+		href : m[0] || '',
+		protocol : m[1] || '',
+		authority: m[2] || '',
+		host : m[3] || '',
+		hostname : m[4] || '',
+		port : m[5] || '',
+		pathname : m[6] || '',
+		search : m[7] || '',
+		hash : m[8] || ''
+	    } : null);
+	}
+ 
+	function absolutizeURI(base, href) {// RFC 3986
+ 
+	    function removeDotSegments(input) {
+		var output = [];
+		input.replace(/^(\.\.?(\/|$))+/, '')
+		    .replace(/\/(\.(\/|$))+/g, '/')
+		    .replace(/\/\.\.$/, '/../')
+		    .replace(/\/?[^\/]*/g, function (p) {
+			if (p === '/..') {
+			    output.pop();
+			} else {
+			    output.push(p);
+			}
+		    });
+		return output.join('').replace(/^\//, input.charAt(0) === '/' ? '/' : '');
+	    }
+ 
+	    href = parseURI(href || '');
+	    base = parseURI(base || '');
+ 
+	    return !href || !base ? null : (href.protocol || base.protocol) +
+		(href.protocol || href.authority ? href.authority : base.authority) +
+		removeDotSegments(href.protocol || href.authority || href.pathname.charAt(0) === '/' ? href.pathname : (href.pathname ? ((base.authority && !base.pathname ? '/' : '') + base.pathname.slice(0, base.pathname.lastIndexOf('/') + 1) + href.pathname) : base.pathname)) +
+		(href.protocol || href.authority || href.pathname ? href.search : (href.search || base.search)) +
+		href.hash;
+	}
+
+	/* Remove local anchor if necessary */
+	if (spec.indexOf("#") != -1) {
+	    spec = spec.substr(0, spec.indexOf ("#"));
+	}
+
+	/* Create the proper absolute zim:// url */
 	var uri = Components.classes["@mozilla.org/network/simple-uri;1"]
-	.createInstance (Components.interfaces.nsIURI);
-	
-	/* Handle anchor links */
-	if (baseURI instanceof Components.interfaces.nsIURI && spec.indexOf ("#") == 0) {
-	    var sh = baseURI.spec.indexOf ("#");
-	    if (sh != -1) {
-		var nb = baseURI.spec.substr (0, sh);
-	    	uri.spec = nb+spec;
-	    } else
-	    	uri.spec = baseURI.spec+spec;
+	    .createInstance(Components.interfaces.nsIURI);
+	if (baseURI instanceof Components.interfaces.nsIURI) {
+	    if (spec[0] == '/') {
+		uri.spec = 'zim:/' + spec;
+	    } else if (spec[1] == '/' && /* This is a hack to work well with ZIM files with wrong urls */ 
+		       (spec[0] == 'A' || spec[0] == 'I' || spec[0] == '-')) {
+		uri.spec = 'zim://' + spec;
+	    } else {
+		uri.spec = absolutizeURI(baseURI.spec, spec);
+	    }
 	} else {
-	    var prefix = spec.indexOf("zim://");
-	    if (prefix == -1) {
-		if (spec.length < 2 || spec[1] != '/' && spec[0] != '/') {
-	            spec = 'A/' + spec;
-		}
-		uri.spec = "zim://" + spec;
-	    } else
-		uri.spec = spec;	    
-	} 
-	
-	uri.spec = uri.spec.replace(/\+/g, "%20");
+	    uri.spec = spec;
+	}
+
 	return uri;
     },
     
