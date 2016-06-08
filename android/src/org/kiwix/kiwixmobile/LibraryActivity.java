f5a6bcd16b6ccbcf1d31e3f7e934e8f45d04f281
Tamas Foldenyi <ftomi@ftomi-macbookpro2.roam.corp.google.com>
2016-06-08 17:10:36 +0200

    search

--- android/src/org/kiwix/kiwixmobile/LibraryActivity.java
+++ android/src/org/kiwix/kiwixmobile/LibraryActivity.java
@@ -3,16 +3,23 @@ package org.kiwix.kiwixmobile;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.ListView;
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import java.util.List;
+
 import org.kiwix.kiwixmobile.downloader.DownloadIntent;
 import org.kiwix.kiwixmobile.downloader.DownloadService;
 import org.kiwix.kiwixmobile.library.LibraryAdapter;
 import org.kiwix.kiwixmobile.library.entity.LibraryNetworkEntity;
 import org.kiwix.kiwixmobile.network.KiwixService;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import rx.android.schedulers.AndroidSchedulers;
 
 public class LibraryActivity extends AppCompatActivity {
@@ -22,19 +29,22 @@ public class LibraryActivity extends AppCompatActivity {
 
   private KiwixService kiwixService;
   private List<LibraryNetworkEntity.Book> books;
+  private LibraryAdapter libraryAdapter;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_library);
     ButterKnife.bind(this);
-    setSupportActionBar(toolbar);
+    setUpToolbar();
+
     kiwixService = ((KiwixApplication) getApplication()).getKiwixService();
     kiwixService.getLibrary()
         .observeOn(AndroidSchedulers.mainThread())
         .subscribe(library -> {
           books = library.getBooks();
-          libraryList.setAdapter(new LibraryAdapter(LibraryActivity.this, books));
+          libraryAdapter = new LibraryAdapter(LibraryActivity.this, books);
+          libraryList.setAdapter(libraryAdapter);
         });
 
     libraryList.setOnItemClickListener(
@@ -43,5 +53,37 @@ public class LibraryActivity extends AppCompatActivity {
           service.putExtra(DownloadIntent.DOWNLOAD_URL_PARAMETER, books.get(position).getUrl());
           startService(service);
         });
+  }
+
+  private void setUpToolbar() {
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+    getSupportActionBar().setHomeButtonEnabled(true);
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    MenuInflater inflater = getMenuInflater();
+    inflater.inflate(R.menu.menu_library_search, menu);
+
+    SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+    searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+      @Override
+      public boolean onQueryTextSubmit(String s) {
+        return false;
+      }
+
+      @Override
+      public boolean onQueryTextChange(String s) {
+        Log.w("xxx", "onQueryTextChange: " + s);
+        if (libraryAdapter != null) {
+          libraryAdapter.getFilter().filter(s);
+        }
+        return true;
+      }
+    });
+
+    return true;
   }
 }
